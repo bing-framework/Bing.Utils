f7883b4c6a1bd94d5970850e4da17c486e1feefb
jian玄冰 <jianxuanhuo1@126.com>
2021-04-08 10:40:59 +0800

    feat: 新增 获取异常详情 方法

--- src/Bing.Utils/Bing/Exceptions/ExceptionHelper.cs
+++ src/Bing.Utils/Bing/Exceptions/ExceptionHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.ExceptionServices;
+using System.Text;
 
 namespace Bing.Exceptions
 {
@@ -18,6 +19,26 @@ namespace Bing.Exceptions
             // The code cannot ever get here. We just return a value to work around a badly-designed API (ExceptionDispatchInfo.Throw):
             //  https://connect.microsoft.com/VisualStudio/feedback/details/689516/exceptiondispatchinfo-api-modifications (http://www.webcitation.org/6XQ7RoJmO)
             return exception;
+        }
+
+        /// <summary>
+        /// 获取异常详情
+        /// </summary>
+        /// <param name="exception">异常</param>
+        /// <returns>格式化之后的异常信息</returns>
+        public static string GetExceptionDetail(Exception exception)
+        {
+            var detail = new StringBuilder();
+            detail.Append(@"***************************************");
+            detail.AppendFormat(@" 异常发生时间： {0} ", DateTime.Now);
+            detail.AppendFormat(@" 异常类型： {0} ", exception.HResult);
+            detail.AppendFormat(@" 导致当前异常的 Exception 实例： {0} ", exception.InnerException);
+            detail.AppendFormat(@" 导致异常的应用程序或对象的名称： {0} ", exception.Source);
+            detail.AppendFormat(@" 引发异常的方法： {0} ", exception.TargetSite);
+            detail.AppendFormat(@" 异常堆栈信息： {0} ", exception.StackTrace);
+            detail.AppendFormat(@" 异常消息： {0} ", exception.Message);
+            detail.Append(@"***************************************");
+            return detail.ToString();
         }
     }
 }
