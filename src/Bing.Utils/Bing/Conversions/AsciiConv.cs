65bc96c02039353c548aaf7e3fd573dbf40d3c32
jian玄冰 <jianxuanhuo1@126.com>
2021-04-12 09:42:07 +0800

    style: 调整 Exception 扩展位置

---
+++ src/Bing.Utils/Bing/Conversions/AsciiConv.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace Bing.Conversions
+{
+    /// <summary>
+    /// ASCII 转换帮助类
+    /// </summary>
+    internal static class AsciiConvHelper
+    {
+        /// <summary>
+        /// 字符转ASCII
+        /// </summary>
+        /// <param name="char">字符</param>
+        public static byte CharToAscii(char @char) => (byte) @char;
+
+        /// <summary>
+        /// 字符串转ASCII
+        /// </summary>
+        /// <param name="string">字符串</param>
+        public static byte[] StringToAscii(string @string) => Encoding.ASCII.GetBytes(@string);
+
+        /// <summary>
+        /// ASCII转字符
+        /// </summary>
+        /// <param name="byte">ASCII字节</param>
+        public static char AsciiToChar(byte @byte) => (char) @byte;
+
+        /// <summary>
+        /// ASCII转字符串
+        /// </summary>
+        /// <param name="bytes">ASCII字节数组</param>
+        public static string AsciiToString(byte[] bytes) => Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+    }
+
+    /// <summary>
+    /// ASCII转换器
+    /// </summary>
+    public static class AsciiConv
+    {
+        /// <summary>
+        /// 将 byte[] 转换为 ASCII <see cref="string"/>
+        /// </summary>
+        /// <param name="bytes">字节数组</param>
+        /// <example>in: new byte[] {65, 66, 67}; out: ABC</example>
+        public static string BytesToAsciiString(byte[] bytes) => AsciiConvHelper.AsciiToString(bytes);
+
+        /// <summary>
+        /// 将 ASCII <see cref="string"/> 转换为 byte[]
+        /// </summary>
+        /// <param name="asciiStr">ASCII字符串</param>
+        /// <example>in: ABC; out: new byte[] {65, 66, 67}</example>
+        public static byte[] AsciiStringToBytes(string asciiStr) => AsciiConvHelper.StringToAscii(asciiStr);
+    }
+}
--- src/Bing.Utils/Bing/Exceptions/ExceptionHelper.cs
+++ src/Bing.Utils/Bing/Exceptions/ExceptionHelper.cs
@@ -41,4 +41,62 @@ namespace Bing.Exceptions
             return detail.ToString();
         }
     }
+
+    /// <summary>
+    /// Bing <see cref="Exception"/> 扩展
+    /// </summary>
+    public static class ExceptionHelperExtensions
+    {
+        /// <summary>
+        /// 解包
+        /// </summary>
+        /// <param name="ex">异常</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static Exception Unwrap(this Exception ex)
+        {
+            if (ex is null)
+                throw new ArgumentNullException(nameof(ex));
+            while (ex.InnerException != null)
+                ex = ex.InnerException;
+            return ex;
+        }
+
+        /// <summary>
+        /// 解包
+        /// </summary>
+        /// <param name="ex">异常</param>
+        /// <param name="untilType">截止类型</param>
+        /// <param name="mayDerivedClass">是否允许派生类</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static Exception Unwrap(this Exception ex, Type untilType, bool mayDerivedClass = true)
+        {
+            if (ex is null)
+                throw new ArgumentNullException(nameof(ex));
+            if (untilType is null)
+                throw new ArgumentNullException(nameof(untilType));
+            if (!untilType.IsSubclassOf(typeof(Exception)))
+                throw new ArgumentException($"Type '{untilType}' does not devide from {typeof(Exception)}", nameof(untilType));
+
+            var exception = ex;
+            return exception.GetType() == untilType || mayDerivedClass && exception.GetType().IsSubclassOf(untilType)
+                ? exception
+                : exception.InnerException is null
+                    ? null
+                    : Unwrap(exception.InnerException, untilType, mayDerivedClass);
+        }
+
+        /// <summary>
+        /// 解包
+        /// </summary>
+        /// <typeparam name="TException">异常类型</typeparam>
+        /// <param name="ex">异常</param>
+        public static Exception Unwrap<TException>(this Exception ex) where TException : Exception => ex.Unwrap(Reflection.Types.Of<TException>());
+
+        /// <summary>
+        /// 解包并返回消息
+        /// </summary>
+        /// <param name="ex">异常</param>
+        public static string ToUnwrappedString(this Exception ex) => ex.Unwrap().Message;
+    }
 }
