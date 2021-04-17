a5d0df316cd262e38a055e4dc08c6d99f0957b40
jian玄冰 <jianxuanhuo1@126.com>
2021-04-17 18:12:02 +0800

    feat: 新增 类型转换操作

---
+++ src/Bing.Utils/Bing/Reflection/TypeConv.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bing.Reflection
+{
+    /// <summary>
+    /// 类型转换 操作
+    /// </summary>
+    public static class TypeConv
+    {
+        /// <summary>
+        /// 将可为空的类型转换为基础类型
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <returns>基础类型</returns>
+        public static Type GetNonNullableType(Type type)
+        {
+            if (type is null)
+                return null;
+
+            if (type.IsArray)
+                return GetNonNullableType(type.GetElementType())?.MakeArrayType();
+
+            if (type.IsGenericType)
+            {
+                var genericTypeDefinition = type.GetGenericTypeDefinition();
+
+                if (genericTypeDefinition == typeof(Nullable<>))
+                    return type.GetGenericArguments()[0];
+
+                if (genericTypeDefinition == typeof(KeyValuePair<,>))
+                {
+                    var baseType = typeof(KeyValuePair<,>);
+                    var args = type.GetGenericArguments();
+                    return baseType.MakeGenericType(args[0], GetNonNullableType(args[1]));
+                }
+
+                if (IsCollectionImplementation(type, out var argumentType, out var dictionaryType))
+                {
+                    var baseType = type.GetGenericTypeDefinition();
+                    return baseType.MakeGenericType(argumentType);
+                }
+
+                if (dictionaryType)
+                {
+                    var args = type.GetGenericArguments();
+                    var baseType = type.GetGenericTypeDefinition();
+                    return baseType.MakeGenericType(args[0], GetNonNullableType(args[1]));
+                }
+            }
+
+            return type;
+
+            // 是否集合接口实现类型
+            bool IsCollectionImplementation(Type ti, out Type at, out bool dt)
+            {
+                at = null;
+                dt = false;
+                var argumentTypes = ti.GetGenericArguments();
+
+                var ret = false;
+
+                switch (argumentTypes.Length)
+                {
+                    case 1:
+                        {
+                            // List<T>, IList<T>
+                            var allInterfaces = ti.GetInterfaces();
+                            if (allInterfaces.Any(x => x == typeof(ICollection) || x == typeof(IEnumerable)))
+                            {
+                                at = GetNonNullableType(argumentTypes[0]);
+                                dt = false;
+                                ret = true;
+                            }
+                            else
+                            {
+                                at = null;
+                                dt = false;
+                            }
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            // Dictionary<TKey, TValue>, IDictionary<TKey, TValue>
+                            var allInterfaces = ti.GetInterfaces();
+                            if (allInterfaces.Contains(typeof(IDictionary)))
+                            {
+                                // Dictionary<TKey, TValue>
+                                at = null;
+                                dt = true;
+                            }
+                            else
+                            {
+                                //ICollection<KeyValuePair<TKey, TValue>>
+                                var pairType = typeof(KeyValuePair<,>).MakeGenericType(argumentTypes[0], argumentTypes[1]);
+                                var collType = typeof(ICollection<>).MakeGenericType(pairType);
+                                if (allInterfaces.Contains(collType))
+                                {
+                                    at = null;
+                                    dt = true;
+                                }
+                            }
+                            break;
+                        }
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// 将可为空的类型转换为基础类型
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <returns>基础类型</returns>
+        public static Type GetNonNullableType<T>() => GetNonNullableType(typeof(T));
+    }
+}
---
+++ tests/Bing.Utils.Tests/ConvUT/TypeConvTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bing.Reflection;
+using Shouldly;
+using Xunit;
+
+namespace Bing.Utils.Tests.ConvUT
+{
+    /// <summary>
+    /// 类型转换操作单元测试
+    /// </summary>
+    [Trait("TypeUT", "TypeConv")]
+    public class TypeConvTests
+    {
+        /// <summary>
+        /// 测试 - 可空基础类型转换为不可空基础类型
+        /// </summary>
+        [Fact]
+        public void Test_NullableTypeToNonNullableType()
+        {
+            TypeConv.GetNonNullableType(typeof(object)).ShouldBe(typeof(object));
+            TypeConv.GetNonNullableType(null).ShouldBeNull();
+
+            TypeConv.GetNonNullableType(typeof(byte)).ShouldBe(typeof(byte));
+            TypeConv.GetNonNullableType(typeof(byte?)).ShouldBe(typeof(byte));
+            TypeConv.GetNonNullableType(typeof(sbyte)).ShouldBe(typeof(sbyte));
+            TypeConv.GetNonNullableType(typeof(sbyte?)).ShouldBe(typeof(sbyte));
+            TypeConv.GetNonNullableType(typeof(short)).ShouldBe(typeof(short));
+            TypeConv.GetNonNullableType(typeof(short?)).ShouldBe(typeof(short));
+            TypeConv.GetNonNullableType(typeof(ushort)).ShouldBe(typeof(ushort));
+            TypeConv.GetNonNullableType(typeof(ushort?)).ShouldBe(typeof(ushort));
+            TypeConv.GetNonNullableType(typeof(int)).ShouldBe(typeof(int));
+            TypeConv.GetNonNullableType(typeof(int?)).ShouldBe(typeof(int));
+            TypeConv.GetNonNullableType(typeof(uint)).ShouldBe(typeof(uint));
+            TypeConv.GetNonNullableType(typeof(uint?)).ShouldBe(typeof(uint));
+            TypeConv.GetNonNullableType(typeof(long)).ShouldBe(typeof(long));
+            TypeConv.GetNonNullableType(typeof(long?)).ShouldBe(typeof(long));
+            TypeConv.GetNonNullableType(typeof(ulong)).ShouldBe(typeof(ulong));
+            TypeConv.GetNonNullableType(typeof(ulong?)).ShouldBe(typeof(ulong));
+
+            TypeConv.GetNonNullableType(typeof(float)).ShouldBe(typeof(float));
+            TypeConv.GetNonNullableType(typeof(float?)).ShouldBe(typeof(float));
+            TypeConv.GetNonNullableType(typeof(double)).ShouldBe(typeof(double));
+            TypeConv.GetNonNullableType(typeof(double?)).ShouldBe(typeof(double));
+            TypeConv.GetNonNullableType(typeof(decimal)).ShouldBe(typeof(decimal));
+            TypeConv.GetNonNullableType(typeof(decimal?)).ShouldBe(typeof(decimal));
+
+            TypeConv.GetNonNullableType(typeof(string)).ShouldBe(typeof(string));
+            TypeConv.GetNonNullableType(typeof(char)).ShouldBe(typeof(char));
+            TypeConv.GetNonNullableType(typeof(char?)).ShouldBe(typeof(char));
+            TypeConv.GetNonNullableType(typeof(bool)).ShouldBe(typeof(bool));
+            TypeConv.GetNonNullableType(typeof(bool?)).ShouldBe(typeof(bool));
+
+            TypeConv.GetNonNullableType(typeof(DateTime)).ShouldBe(typeof(DateTime));
+            TypeConv.GetNonNullableType(typeof(DateTime?)).ShouldBe(typeof(DateTime));
+            TypeConv.GetNonNullableType(typeof(DateTimeOffset)).ShouldBe(typeof(DateTimeOffset));
+            TypeConv.GetNonNullableType(typeof(DateTimeOffset?)).ShouldBe(typeof(DateTimeOffset));
+            TypeConv.GetNonNullableType(typeof(TimeSpan)).ShouldBe(typeof(TimeSpan));
+            TypeConv.GetNonNullableType(typeof(TimeSpan?)).ShouldBe(typeof(TimeSpan));
+
+            TypeConv.GetNonNullableType(typeof(Guid)).ShouldBe(typeof(Guid));
+            TypeConv.GetNonNullableType(typeof(Guid?)).ShouldBe(typeof(Guid));
+            TypeConv.GetNonNullableType(typeof(ValueTuple)).ShouldBe(typeof(ValueTuple));
+            TypeConv.GetNonNullableType(typeof(ValueTuple?)).ShouldBe(typeof(ValueTuple));
+            TypeConv.GetNonNullableType(typeof(ValueTuple<>)).ShouldBe(typeof(ValueTuple<>));
+
+            TypeConv.GetNonNullableType(typeof(Task)).ShouldBe(typeof(Task));
+            TypeConv.GetNonNullableType(typeof(Task<>)).ShouldBe(typeof(Task<>));
+            TypeConv.GetNonNullableType(typeof(ValueTask)).ShouldBe(typeof(ValueTask));
+            TypeConv.GetNonNullableType(typeof(ValueTask?)).ShouldBe(typeof(ValueTask));
+            TypeConv.GetNonNullableType(typeof(ValueTask<>)).ShouldBe(typeof(ValueTask<>));
+        }
+
+        /// <summary>
+        /// 测试 - 可空数组类型转换为不可空数组类型
+        /// </summary>
+        [Fact]
+        public void Test_NullableTypeArrayToNonNullableTypeArray()
+        {
+            TypeConv.GetNonNullableType(typeof(byte)).ShouldBe(typeof(byte));
+            TypeConv.GetNonNullableType(typeof(byte?[])).ShouldBe(typeof(byte[]));
+            TypeConv.GetNonNullableType(typeof(List<byte>)).ShouldBe(typeof(List<byte>));
+            TypeConv.GetNonNullableType(typeof(List<byte?>)).ShouldBe(typeof(List<byte>));
+            TypeConv.GetNonNullableType(typeof(Dictionary<string, int>)).ShouldBe(typeof(Dictionary<string, int>));
+            TypeConv.GetNonNullableType(typeof(Dictionary<string, int?>)).ShouldBe(typeof(Dictionary<string, int>));
+        }
+
+        /// <summary>
+        /// 测试 - 可空数组接口类型转换为不可空数据接口类型
+        /// </summary>
+        [Fact]
+        public void Test_NullableTypeInterfaceToNonNullableInterface()
+        {
+            TypeConv.GetNonNullableType(typeof(IList<byte>)).ShouldBe(typeof(IList<byte>));
+            TypeConv.GetNonNullableType(typeof(IList<byte?>)).ShouldBe(typeof(IList<byte>));
+            TypeConv.GetNonNullableType(typeof(IDictionary<string, int>)).ShouldBe(typeof(IDictionary<string, int>));
+            TypeConv.GetNonNullableType(typeof(IDictionary<string, int?>)).ShouldBe(typeof(IDictionary<string, int>));
+        }
+    }
+}
