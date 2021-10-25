c3d9ac11d30a24dcafce60341f3db08743c6b93d
jian玄冰 <jianxuanhuo1@126.com>
2021-10-25 17:42:48 +0800

    feat: 新增Id生成器组件

---
+++ src/Bing.Utils.IdGenerators/Bing/IdGenerators/SnowflakeProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace Bing.IdGenerators
+{
+    /// <summary>
+    /// 雪花算法Id 生成器
+    /// </summary>
+    public static class SnowflakeProvider
+    {
+        /// <summary>
+        /// 生成
+        /// </summary>
+        /// <param name="workerId">机器ID</param>
+        /// <param name="dataCenterId">数据标识ID</param>
+        /// <param name="sequence">序列号标识</param>
+        public static SnowflakeIdWorker Create(long workerId, long dataCenterId, long sequence = 0L) => new SnowflakeIdWorker(workerId, dataCenterId, sequence);
+    }
+}
