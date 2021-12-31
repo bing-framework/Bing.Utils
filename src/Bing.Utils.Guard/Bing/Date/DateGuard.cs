70181827f47701b3f2509f565fe5bf4579e5b25b
jian玄冰 <jianxuanhuo1@126.com>
2021-12-31 11:02:50 +0800

    feat: 新增守护库

---
+++ src/Bing.Utils.Guard/Bing/Date/DateGuard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bing.Date
+{
+    /// <summary>
+    /// 日期守护
+    /// </summary>
+    public static class DateGuard
+    {
+        public static void ShouldBeValid(DateTime argument, string argumentName, string message = null)
+        {
+
+        }
+    }
+}
