a843734441dbc24ebc42550136766adc379984dd
jian玄冰 <jianxuanhuo1@126.com>
2022-08-14 16:28:46 +0800

    feat: 新增 只读集合工具

---
+++ src/Bing.Utils.Collections/Bing/Collections/Internals/AppendedReadOnlyCollection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Bing.Collections.Internals
+{
+    /// <summary>
+    /// 可追加的只读集合
+    /// </summary>
+    /// <typeparam name="T">类型</typeparam>
+    internal class AppendedReadOnlyCollection<T> : IReadOnlyCollection<T>
+    {
+        /// <summary>
+        /// 集合
+        /// </summary>
+        private readonly IEnumerable<T> _enumerable;
+
+        /// <summary>
+        /// 初始化一个<see cref="AppendedReadOnlyCollection{T}"/>类型的实例
+        /// </summary>
+        /// <param name="root">只读集合</param>
+        /// <param name="item">追加项</param>
+        internal AppendedReadOnlyCollection(IReadOnlyCollection<T> root, T item)
+        {
+            _enumerable = ReadOnlyCollsHelper.Append(root, item);
+            Count = root.Count + 1;
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<T> GetEnumerator() => _enumerable.GetEnumerator();
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <inheritdoc />
+        public int Count { get; }
+    }
+}
--- src/Bing.Utils.Collections/Bing/Collections/ReadOnlyColls.cs
+++ src/Bing.Utils.Collections/Bing/Collections/ReadOnlyColls.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using Bing.Collections.Internals;
 
 namespace Bing.Collections
 {
@@ -24,11 +26,11 @@ namespace Bing.Collections
         }
 
         /// <summary>
-        /// 追加
+        /// 附加
         /// </summary>
         /// <typeparam name="T">类型</typeparam>
         /// <param name="source">源集合</param>
-        /// <param name="items">追加项</param>
+        /// <param name="items">附加项</param>
         /// <exception cref="ArgumentNullException"></exception>
         public static IEnumerable<T> Append<T>(IEnumerable<T> source, params T[] items)
         {
@@ -38,8 +40,67 @@ namespace Bing.Collections
         }
     }
 
-    public class ReadOnlyColls
+    /// <summary>
+    /// 只读集合工具
+    /// </summary>
+    public static partial class ReadOnlyColls
     {
+        /// <summary>
+        /// 附加
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="source">源集合</param>
+        /// <param name="item">附加项</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IReadOnlyCollection<T> Append<T>(IReadOnlyCollection<T> source, T item)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            return new AppendedReadOnlyCollection<T>(source, item);
+        }
 
+        /// <summary>
+        /// 获取一个空的只读集合
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        public static ReadOnlyCollection<T> Empty<T>() => EmptyReadOnlyCollectionSingleton<T>.Instance;
+
+        /// <summary>
+        /// 空的只读集合单例
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        private static class EmptyReadOnlyCollectionSingleton<T>
+        {
+            /// <summary>
+            /// 实例
+            /// </summary>
+            internal static readonly ReadOnlyCollection<T> Instance = new(Array.Empty<T>());
+        }
+
+        //public static IReadOnlyList<T> OfList<T>(params T[] @params)
+        //{
+        //}
+
+        //public static IReadOnlyList<T> OfList<T>(params IEnumerable<T>[] listParams)
+        //{
+
+        //}
+
+        //public static IReadOnlyList<T> OfList<T>(IEnumerable<T> list, params IEnumerable<T>[] listParams)
+        //{
+
+        //}
+    }
+
+    public static class ReadOnlyCollsExtensions
+    {
+        /// <summary>
+        /// 附加
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="source">源集合</param>
+        /// <param name="item">附加项</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IReadOnlyCollection<T> Append<T>(this IReadOnlyCollection<T> source, T item) => ReadOnlyColls.Append(source, item);
     }
 }
