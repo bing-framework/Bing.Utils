f882f6a05001302c27c6a89fb7cfdef5be4d00df
jian玄冰 <jianxuanhuo1@126.com>
2022-08-20 16:34:59 +0800

    feat: 新增字符串工具

--- src/Bing.Utils/Bing/IdUtils/GuidImplements/SequentialGuidTypes.cs
+++ src/Bing.Utils/Bing/IdUtils/GuidImplements/SequentialGuidTypes.cs
@@ -8,16 +8,19 @@
         /// <summary>
         /// 生成的 GUID 按照字符串顺序排列
         /// </summary>
+        /// <remarks>主要用于 MySQL 和 PostgreSQL</remarks>
         SequentialAsString,
 
         /// <summary>
         /// 生成的 GUID 按照二进制的顺序排列
         /// </summary>
+        /// <remarks>主要用于 Oracle</remarks>
         SequentialAsBinary,
 
         /// <summary>
         /// 生成的GUID 像SQL Server, 按照末尾部分排列
         /// </summary>
+        /// <remarks>主要用于 SQL Server</remarks>
         SequentialAtEnd
     }
 }
---
+++ src/Bing.Utils/Bing/Text/Strings.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Bing.Text
+{
+    /// <summary>
+    /// 字符串工具
+    /// </summary>
+    public static partial class Strings
+    {
+        
+        #region Filter
+
+        /// <summary>
+        /// 过滤为字符
+        /// </summary>
+        /// <param name="text">文本</param>
+        /// <param name="predicate">条件</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<char> FilterByChar(string text, Func<char, bool> predicate) => text.ToCharArray().Where(predicate);
+
+        /// <summary>
+        /// 只获取字母和数字
+        /// </summary>
+        /// <param name="text">文本</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<char> FilterForNumbersAndLetters(string text)
+        {
+            return FilterByChar(text, LocalCheck);
+
+            bool LocalCheck(char @char) => (@char >= 'a' && @char <= 'z') || (@char >= 'A' && @char <= 'Z') || (@char >= '0' && @char <= '9');
+        }
+
+        /// <summary>
+        /// 只获取数字
+        /// </summary>
+        /// <param name="text">文本</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<char> FilterForNumbers(string text)
+        {
+            return FilterByChar(text, LocalCheck);
+
+            bool LocalCheck(char @char) => @char >= '0' && @char <= '9';
+        }
+
+        /// <summary>
+        /// 只获取字母
+        /// </summary>
+        /// <param name="text">文本</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<char> FilterForLetters(string text)
+        {
+            return FilterByChar(text, LocalCheck);
+
+            bool LocalCheck(char @char) => (@char >= 'a' && @char <= 'z') || (@char >= 'A' && @char <= 'Z');
+        }
+
+        #endregion
+
+        #region Merge
+
+        /// <summary>
+        /// 将字符集合合并为一个字符串
+        /// </summary>
+        /// <param name="chars">字符集合</param>
+        public static string Merge(IEnumerable<char> chars)
+        {
+            var sb = new StringBuilder();
+            if(chars is not null)
+                foreach (var val in chars)
+                    sb.Append(val);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 将字符集合合并为一个字符串
+        /// </summary>
+        /// <param name="text">文本</param>
+        /// <param name="strings">字符集合</param>
+        public static string Merge(string text, params string[] strings)
+        {
+            var sb = new StringBuilder();
+            sb.Append(text);
+            if(strings is not null)
+                foreach (var val in strings)
+                    sb.Append(val);
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
