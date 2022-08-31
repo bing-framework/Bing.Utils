f16c78b2d43b8563dad2ac24c0836ca93351f57b
jian玄冰 <jianxuanhuo1@126.com>
2022-08-31 10:45:00 +0800

    fix: 还原 ForEach 操作

--- src/Bing.Utils/Bing/Collections/Extensions/Enumerable/Extensions.Enumerable.ForEach.cs
+++ src/Bing.Utils/Bing/Collections/Extensions/Enumerable/Extensions.Enumerable.ForEach.cs
@@ -11,22 +11,22 @@ namespace Bing.Collections
     /// </summary>
     public static partial class EnumerableExtensions
     {
-        ///// <summary>
-        ///// 对指定集合中的每个元素执行指定操作
-        ///// </summary>
-        ///// <typeparam name="T">类型</typeparam>
-        ///// <param name="enumerable">值</param>
-        ///// <param name="action">操作</param>
-        ///// <exception cref="ArgumentNullException">源集合对象为空、操作表达式为空</exception>
-        //public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
-        //{
-        //    if (enumerable == null)
-        //        throw new ArgumentNullException(nameof(enumerable), $@"源{typeof(T).Name}集合对象不可为空！");
-        //    if (action == null)
-        //        throw new ArgumentNullException(nameof(action), @"操作表达式不可为空！");
-        //    foreach (var item in enumerable)
-        //        action(item);
-        //}
+        /// <summary>
+        /// 对指定集合中的每个元素执行指定操作
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="enumerable">值</param>
+        /// <param name="action">操作</param>
+        /// <exception cref="ArgumentNullException">源集合对象为空、操作表达式为空</exception>
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            if (enumerable == null)
+                throw new ArgumentNullException(nameof(enumerable), $@"源{typeof(T).Name}集合对象不可为空！");
+            if (action == null)
+                throw new ArgumentNullException(nameof(action), @"操作表达式不可为空！");
+            foreach (var item in enumerable)
+                action(item);
+        }
 
         /// <summary>
         /// 对指定集合中的每个元素执行指定操作
