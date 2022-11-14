7aa4f41f83f2e6e5d17620eee1f760a4a1af8dea
jian玄冰 <jianxuanhuo1@126.com>
2022-11-14 11:31:52 +0800

    test: 新增 FTP客户端 单元测试

--- tests/Bing.Utils.Tests/Net/FTP/FtpClientTest.cs
+++ tests/Bing.Utils.Tests/Net/FTP/FtpClientTest.cs
@@ -1,5 +1,9 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using Bing.Extensions;
+using Bing.IO;
 using Bing.Net.FTP;
+using FluentFTP.Helpers;
 using Xunit.Abstractions;
 
 namespace Bing.Utils.Tests.Net.FTP;
@@ -11,12 +15,26 @@ public class FtpClientTest : TestBase
     /// </summary>
     private readonly IFtpClient _client;
 
+    // 测试路径
+    private const string TEST_FILES_PATH = "Resources/TestFiles";
+    private const string ACTIVE_FILE_PATH = "Resources/ActiveTestFolder";
+
+    // 测试文件
+    private const string TEST_FILE_1 = "Text.txt";
+    private const string TEST_FILE_2 = "测试文件1.txt";
+    private const string TEST_FOLDER_1 = "TestSubFolder";
+    private const string TEST_DOWNLOAD_FOLDER = "DownloadFolder";
+
+    private readonly DirectoryInfo _activeFolder;
+
     /// <summary>
     /// 初始化一个<see cref="TestBase"/>类型的实例
     /// </summary>
     public FtpClientTest(ITestOutputHelper output) : base(output)
     {
         _client = new FtpClient("10.186.100.90", 2121, "erp01", "Erp@2022", false, EncryptionType.None);
+        DirectoryHelper.CreateIfNotExists(GetTestFilePath(ACTIVE_FILE_PATH));
+        _activeFolder = new DirectoryInfo(GetTestFilePath(ACTIVE_FILE_PATH));
     }
 
     /// <summary>
@@ -33,11 +51,205 @@ public class FtpClientTest : TestBase
     /// <summary>
     /// 测试 - 上传文件
     /// </summary>
-    [Fact]
-    public void Test_UploadFile()
+    [Theory]
+    [InlineData(TEST_FILE_1)]
+    [InlineData(TEST_FILE_2)]
+    public void Test_UploadFile(string fileName)
     {
         _client.Connect();
-        var result = _client.UploadFile("export\\test", Path.Combine("D:\\", "测试导出_20191225120408.xlsx"));
+        var result = _client.UploadFile(GetTestFilePath(TEST_FILES_PATH, fileName), fileName);
+        Assert.True(result);
+        result = _client.DeleteFile(fileName);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 上传文件 - 工作目录
+    /// </summary>
+    [Theory]
+    [InlineData(TEST_FILE_1)]
+    [InlineData(TEST_FILE_2)]
+    public void Test_UploadFile_SubFolder(string fileName)
+    {
+        _client.Connect();
+        _client.CreateDirectory(TEST_FOLDER_1);
+        _client.SetCurrentDirectory(TEST_FOLDER_1);
+        var result = _client.UploadFile(GetTestFilePath(TEST_FILES_PATH, fileName), fileName);
+        Assert.True(result);
+        result = _client.DeleteFile(fileName);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 上传文件流 - 工作目录
+    /// </summary>
+    [Theory]
+    [InlineData(TEST_FILE_1)]
+    [InlineData(TEST_FILE_2)]
+    public void Test_UploadStream_SubFolder(string fileName)
+    {
+        _client.Connect();
+        _client.CreateDirectory(TEST_FOLDER_1);
+        _client.SetCurrentDirectory(TEST_FOLDER_1);
+        using var stream = File.OpenRead(GetTestFilePath(TEST_FILES_PATH, fileName));
+        var result = _client.UploadStream(stream, fileName);
+        Assert.True(result);
+        result = _client.DeleteFile(fileName);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 上传字节数组 - 工作目录
+    /// </summary>
+    [Theory]
+    [InlineData(TEST_FILE_1)]
+    [InlineData(TEST_FILE_2)]
+    public void Test_UploadBytes_SubFolder(string fileName)
+    {
+        _client.Connect();
+        _client.CreateDirectory(TEST_FOLDER_1);
+        _client.SetCurrentDirectory(TEST_FOLDER_1);
+        using var stream = File.OpenRead(GetTestFilePath(TEST_FILES_PATH, fileName));
+        var bytes = stream.GetAllBytes();
+        var result = _client.UploadBytes(bytes, fileName);
+        Assert.True(result);
+        result = _client.DeleteFile(fileName);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 批量上传文件 - 工作目录
+    /// </summary>
+    [Fact]
+    public void Test_UploadFiles_SubFolder()
+    {
+        _client.Connect();
+        _client.CreateDirectory(TEST_FOLDER_1);
+        _client.SetCurrentDirectory(TEST_FOLDER_1);
+        var paths = new List<string>
+        {
+            GetTestFilePath(TEST_FILES_PATH, TEST_FILE_1),
+            GetTestFilePath(TEST_FILES_PATH, TEST_FILE_2),
+        };
+        var remotePath = "test";
+        var result = _client.UploadFiles(paths, remotePath);
+        Assert.True(result);
+        result = _client.DeleteDirectory(remotePath);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 上传文件夹
+    /// </summary>
+    [Fact]
+    public void Test_UploadDirectory_SubFolder()
+    {
+        _client.Connect();
+        _client.CreateDirectory(TEST_FOLDER_1);
+        _client.SetCurrentDirectory(TEST_FOLDER_1);
+        
+        var remotePath = "test";
+        var result = _client.UploadDirectory(GetTestFilePath(TEST_FILES_PATH), remotePath);
+        Assert.True(result);
+        result = _client.DeleteDirectory(remotePath);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 下载文件
+    /// </summary>
+    [Theory]
+    [InlineData(TEST_FILE_1)]
+    [InlineData(TEST_FILE_2)]
+    public void Test_DownloadFile(string fileName)
+    {
+        var result = _client.UploadFile(GetTestFilePath(TEST_FILES_PATH, fileName), fileName);
+        Assert.True(result);
+        var localFilePath = Path.Combine(_activeFolder.FullName, TEST_DOWNLOAD_FOLDER, fileName);
+        if(File.Exists(localFilePath))
+            File.Delete(localFilePath);
+        result = _client.DownloadFile(localFilePath, fileName);
+        Assert.True(result);
+        result = _client.DeleteFile(fileName);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 下载字节数组
+    /// </summary>
+    [Theory]
+    [InlineData(TEST_FILE_1)]
+    [InlineData(TEST_FILE_2)]
+    public void Test_DownloadStream(string fileName)
+    {
+        var result = _client.UploadFile(GetTestFilePath(TEST_FILES_PATH, fileName), fileName);
+        Assert.True(result);
+        var localFilePath = Path.Combine(_activeFolder.FullName, TEST_DOWNLOAD_FOLDER, fileName);
+        if (File.Exists(localFilePath))
+            File.Delete(localFilePath);
+        using var stream = File.Open(localFilePath, FileMode.OpenOrCreate, FileAccess.Write);
+        result = _client.DownloadBytes(out var bytes, fileName);
+        stream.Write(bytes);
+        Assert.True(result);
+        result = _client.DeleteFile(fileName);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 批量下载文件
+    /// </summary>
+    [Fact]
+    public void Test_DownloadFiles()
+    {
+        var paths = new List<string>
+        {
+            GetTestFilePath(TEST_FILES_PATH, TEST_FILE_1),
+            GetTestFilePath(TEST_FILES_PATH, TEST_FILE_2),
+        };
+        var remotePath = "test";
+        var result = _client.UploadFiles(paths, remotePath);
+        Assert.True(result);
+
+        var remotePaths = new List<string>
+        {
+            $"/test/{TEST_FILE_1}",
+            $"/test/{TEST_FILE_2}",
+        };
+        var localPath= Path.Combine(_activeFolder.FullName, TEST_DOWNLOAD_FOLDER);
+        result = _client.DownloadFiles(localPath, remotePaths);
+        Assert.True(result);
+        result = _client.DeleteDirectory(remotePath);
+        Assert.True(result);
+        _client.Dispose();
+    }
+
+    /// <summary>
+    /// 测试 - 下载文件夹
+    /// </summary>
+    [Fact]
+    public void Test_DownloadDirectory()
+    {
+        var paths = new List<string>
+        {
+            GetTestFilePath(TEST_FILES_PATH, TEST_FILE_1),
+            GetTestFilePath(TEST_FILES_PATH, TEST_FILE_2),
+        };
+        var remotePath = "test";
+        var result = _client.UploadFiles(paths, remotePath);
+        Assert.True(result);
+
+        var localPath = Path.Combine(_activeFolder.FullName, TEST_DOWNLOAD_FOLDER);
+        result = _client.DownloadDirectory(localPath, remotePath);
+        Assert.True(result);
+        result = _client.DeleteDirectory(remotePath);
         Assert.True(result);
         _client.Dispose();
     }
--- tests/Bing.Utils.Tests/TestBase.cs
+++ tests/Bing.Utils.Tests/TestBase.cs
@@ -1,4 +1,6 @@
-﻿using Xunit.Abstractions;
+﻿using System.Collections.Generic;
+using System.IO;
+using Xunit.Abstractions;
 
 namespace Bing.Utils.Tests;
 
@@ -16,4 +18,26 @@ public abstract class TestBase
     /// 初始化一个<see cref="TestBase"/>类型的实例
     /// </summary>
     protected TestBase(ITestOutputHelper output) => Output = output;
+
+    /// <summary>
+    /// 获取根路径
+    /// </summary>
+    protected string GetTestRootPath() => Directory.GetCurrentDirectory();
+
+    /// <summary>
+    /// 获取测试文件根路径
+    /// </summary>
+    /// <param name="paths">路径</param>
+    protected string GetTestFilePath(params string[] paths)
+    {
+        var rootPath = GetTestRootPath();
+        var list = new List<string>
+        {
+            rootPath
+        };
+        list.AddRange(paths);
+        var result = Path.Combine(list.ToArray());
+        Output.WriteLine($"文件路径: {result}");
+        return result;
+    }
 }
