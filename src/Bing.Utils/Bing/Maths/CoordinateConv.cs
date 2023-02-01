c45c81be0238dbebbb8424407afb3f8977c9e90e
jian玄冰 <jianxuanhuo1@126.com>
2023-02-01 13:53:28 +0800

    test: 增加 坐标系转换工具类 单元测试

--- src/Bing.Utils/Bing/Maths/CoordinateConv.cs
+++ src/Bing.Utils/Bing/Maths/CoordinateConv.cs
@@ -1,8 +1,28 @@
-﻿namespace Bing.Maths;
+﻿using System.Drawing;
+
+namespace Bing.Maths;
 
 /// <summary>
-/// 坐标轴转换类，UMD魔法代码。
+/// 各坐标系之间的转换工具类，UMD魔法代码。
 /// </summary>
+/// <remarks>
+/// WGS84坐标系：即地球坐标系，国际上通用的坐标系。设备一般包含GPS芯片或者北斗芯片获取的经纬度为WGS84地理坐标系。
+/// 谷歌地图采用的是WGS84地理坐标系（中国范围除外,谷歌中国地图采用的是GCJ02地理坐标系。)
+/// <br /><br />
+/// GCJ02坐标系：即火星坐标系，WGS84坐标系经加密后的坐标系。
+/// 出于国家安全考虑，国内所有导航电子地图必须使用国家测绘局制定的加密坐标系统，即将一个真实的经纬度坐标加密成一个不正确的经纬度坐标。
+/// <br /><br />
+/// BD09坐标系：即百度坐标系，GCJ02坐标系经加密后的坐标系。搜狗坐标系、图吧坐标系等，估计也是在GCJ02基础上加密而成的。
+/// <para>
+/// 高德MapABC地图API 火星坐标<br />
+/// 腾讯搜搜地图API 火星坐标<br />
+/// 阿里云地图API 火星坐标<br />
+/// 灵图51ditu地图API 火星坐标<br />
+/// 百度地图API 百度坐标<br />
+/// 搜狐搜狗地图API 搜狗坐标<br />
+/// 图吧MapBar地图API 图吧坐标<br />
+/// </para>
+/// </remarks>
 public static class CoordinateConv
 {
     /// <summary>
@@ -14,6 +34,11 @@ public static class CoordinateConv
     /// 坐标转换参数：π
     /// </summary>
     private const double PI = 3.1415926535897932384626433832795D;
+
+    // Krasovsky 1940
+    // 长半轴a = 6378245.0, 1/f = 298.3
+    // b = a * (1 - f)
+    // 扁率ee = (a^2 - b^2) / a^2;
 
     /// <summary>
     /// 地球半径（Krasovsky 1940）克拉索夫斯基椭球体
@@ -69,7 +94,7 @@ public static class CoordinateConv
     #region 火星坐标系(GCJ-02)
 
     /// <summary>
-    /// 将 火星坐标系（GCJ-02） 转换为 地球坐标系(WGS-84)
+    /// 将 火星坐标系（GCJ-02） 转换为 地球坐标系(WGS-84)【粗略】
     /// </summary>
     /// <param name="lng">经度坐标</param>
     /// <param name="lat">纬度坐标</param>
@@ -78,6 +103,49 @@ public static class CoordinateConv
     public static Coordinate GCJ02ToWGS84(double lng, double lat)
     {
         return OutOfChina(lng, lat) ? new Coordinate(lng, lat) : new Coordinate(lng, lat).Offset(Offset(lng, lat, false));
+    }
+
+    /// <summary>
+    /// 将 火星坐标系（GCJ-02） 转换为 地球坐标系(WGS-84)【精确】
+    /// </summary>
+    /// <param name="lng">经度坐标</param>
+    /// <param name="lat">纬度坐标</param>
+    /// <returns>地球坐标系(WGS-84)</returns>
+    // ReSharper disable once InconsistentNaming
+    public static Coordinate GCJ02ToWGS84Exactly(double lng, double lat)
+    {
+        if (OutOfChina(lng, lat))
+            return new Coordinate(lng, lat);
+        double initDelta = 0.01;
+        double threshold = 0.000000001;
+        double dLat = initDelta, dLon = initDelta;
+        double mLat = lat - dLat, mLon = lng - dLon;
+        double pLat = lat + dLat, pLon = lng + dLon;
+        double wgsLat, wgsLng, i = 0;
+        while (true)
+        {
+            wgsLat = (mLat + pLat) / 2;
+            wgsLng = (mLon + pLon) / 2;
+            var coordinate = WGS84ToGCJ02(wgsLng, wgsLat);
+            dLon = coordinate.Lng - lng;
+            dLat = coordinate.Lat - lat;
+            if ((Math.Abs(dLat) < threshold) && (Math.Abs(dLon) < threshold))
+                break;
+
+            if (dLat > 0)
+                pLat = wgsLat;
+            else
+                mLat = wgsLat;
+            if (dLon > 0)
+                pLon = wgsLng;
+            else
+                mLon = wgsLng;
+
+            if (++i > 10000)
+                break;
+        }
+
+        return new Coordinate(wgsLng, wgsLat);
     }
 
     /// <summary>
@@ -113,9 +181,9 @@ public static class CoordinateConv
         var y = lat - 0.006;
         var z = Math.Sqrt(x * x + y * y) - 0.00002 * Math.Sin(y * X_PI);
         var theta = Math.Atan2(y, x) - 0.000003 * Math.Cos(x * X_PI);
-        var ggLng = z * Math.Cos(theta);
-        var ggLat = z * Math.Sin(theta);
-        return new Coordinate(ggLng, ggLat);
+        var gcjLng = z * Math.Cos(theta);
+        var gcjLat = z * Math.Sin(theta);
+        return new Coordinate(gcjLng, gcjLat);
     }
 
     /// <summary>
---
+++ tests/Bing.Utils.Tests/Maths/CoordinateConvTest.cs
@@ -0,0 +1,70 @@
+﻿using Bing.Maths;
+
+namespace Bing.Utils.Tests.Maths;
+
+public class CoordinateConvTest
+{
+    /// <summary>
+    /// 经度
+    /// </summary>
+    private const double BASE_LNG = 113.331073;
+
+    /// <summary>
+    /// 纬度
+    /// </summary>
+    private const double BASE_LAT = 23.112214;
+
+    [Theory]
+    [InlineData(113.32453135630236, 23.106450240552665)]// 113.32453135630236,23.106450240552665
+    public void Test_BD09ToGCJ02(double lng, double lat)
+    {
+        var result = CoordinateConv.BD09ToGCJ02(BASE_LNG, BASE_LAT);
+        Assert.Equal(lng, result.Lng);
+        Assert.Equal(lat, result.Lat);
+    }
+
+    [Theory]
+    [InlineData(113.31911008504576, 23.109052036366514)]// 113.31911008504576,23.109052036366514
+    public void Test_BD09ToWGS84(double lng, double lat)
+    {
+        var result = CoordinateConv.BD09ToWGS84(BASE_LNG, BASE_LAT);
+        Assert.Equal(lng, result.Lng);
+        Assert.Equal(lat, result.Lat);
+    }
+
+    [Theory]
+    [InlineData(113.33650811893553, 23.109627055413704)]// 113.33650811893553,23.109627055413704
+    public void Test_WGS84ToGCJ02(double lng, double lat)
+    {
+        var result = CoordinateConv.WGS84ToGCJ02(BASE_LNG, BASE_LAT);
+        Assert.Equal(lng, result.Lng);
+        Assert.Equal(lat, result.Lat);
+    }
+
+    [Theory]
+    [InlineData(113.34306682663143, 23.115290783030787)]// 113.34306682663143,23.115290783030787
+    public void Test_WGS84ToBD09(double lng, double lat)
+    {
+        var result = CoordinateConv.WGS84ToBD09(BASE_LNG, BASE_LAT);
+        Assert.Equal(lng, result.Lng);
+        Assert.Equal(lat, result.Lat);
+    }
+
+    [Theory]
+    [InlineData(113.32563788106448, 23.1148009445863)]// 113.32563788106448,23.1148009445863
+    public void Test_GCJ02ToWGS84(double lng, double lat)
+    {
+        var result = CoordinateConv.GCJ02ToWGS84(BASE_LNG, BASE_LAT);
+        Assert.Equal(lng, result.Lng);
+        Assert.Equal(lat, result.Lat);
+    }
+
+    [Theory]
+    [InlineData(113.33762320331894, 23.117908090871566)]// 113.33762320331894,23.117908090871566
+    public void Test_GCJ02ToBD09(double lng, double lat)
+    {
+        var result = CoordinateConv.GCJ02ToBD09(BASE_LNG, BASE_LAT);
+        Assert.Equal(lng, result.Lng);
+        Assert.Equal(lat, result.Lat);
+    }
+}
