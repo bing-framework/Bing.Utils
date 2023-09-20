960b037596045f955cdafc5157f8b53d3c47818d
jian玄冰 <jianxuanhuo1@126.com>
2023-09-20 16:17:36 +0800

    feat: 新增 HttpResponseMessage 扩展方法

---
+++ src/Bing.Utils.Http/Bing/Http/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+// ReSharper disable once CheckNamespace
+namespace Bing.Http;
+
+/// <summary>
+/// Http响应消息(<see cref="HttpResponseMessage"/>) 扩展
+/// </summary>
+public static class HttpResponseMessageExtensions
+{
+    /// <summary>
+    /// 获取内容类型
+    /// </summary>
+    /// <param name="message">Http响应消息</param>
+    public static string GetContentType(this HttpResponseMessage message) => message?.Content.Headers.ContentType?.MediaType;
+}
