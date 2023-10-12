5aa980c52f981b59230eb43c3052a77eeec69b28
jian玄冰 <jianxuanhuo1@126.com>
2023-10-12 11:11:52 +0800

    feat: 新增 区域文化操作

---
+++ src/Bing.Utils/Bing/Helpers/Culture.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Bing.Helpers;
+
+/// <summary>
+/// 区域文化
+/// </summary>
+public static class Culture
+{
+    /// <summary>
+    /// 获取当前区域文化
+    /// </summary>
+    public static CultureInfo GetCurrentCulture() => CultureInfo.CurrentCulture;
+
+    /// <summary>
+    /// 获取当前UI区域文化
+    /// </summary>
+    public static CultureInfo GetCurrentUICulture() => CultureInfo.CurrentUICulture;
+
+    /// <summary>
+    /// 获取当前区域文化名称
+    /// </summary>
+    public static string GetCurrentCultureName() => CultureInfo.CurrentCulture.Name;
+
+    /// <summary>
+    /// 获取当前UI区域文化名称
+    /// </summary>
+    public static string GetCurrentUICultureName() => CultureInfo.CurrentUICulture.Name;
+
+    /// <summary>
+    /// 获取当前区域文化信息列表，包含所有父区域文化
+    /// </summary>
+    public static List<CultureInfo> GetCurrentCultures() => GetCultures(GetCurrentCulture());
+
+    /// <summary>
+    /// 获取当前UI区域文化信息列表，包含所有父区域文化
+    /// </summary>
+    public static List<CultureInfo> GetCurrentUICultures() => GetCultures(GetCurrentUICulture());
+
+    /// <summary>
+    /// 获取区域文化信息列表，包含所有父区域文化
+    /// </summary>
+    /// <param name="culture">区域文化信息</param>
+    public static List<CultureInfo> GetCultures(CultureInfo culture)
+    {
+        var result = new List<CultureInfo>();
+        if (culture == null)
+            return result;
+        while (culture.Equals(culture.Parent) == false)
+        {
+            result.Add(culture);
+            culture = culture.Parent;
+        }
+        return result;
+    }
+}
---
+++ tests/Bing.Utils.Tests/Helpers/CultureTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using Bing.Helpers;
+
+namespace Bing.Utils.Tests.Helpers;
+
+/// <summary>
+/// 区域文化测试
+/// </summary>
+public class CultureTest
+{
+    /// <summary>
+    /// 测试 - 获取区域文化信息列表
+    /// </summary>
+    [Fact]
+    public void Test_GetCultures()
+    {
+        var culture = new CultureInfo("zh-CN");
+        var cultures = Culture.GetCultures(culture);
+        Assert.Equal(3, cultures.Count);
+        Assert.Equal("zh-CN", cultures[0].Name);
+        Assert.Equal("zh-Hans", cultures[1].Name);
+        Assert.Equal("zh", cultures[2].Name);
+    }
+}
