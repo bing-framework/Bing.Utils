cdb92f26d90f2dcf0a5fdd65716462cea57deedf
jian玄冰 <jianxuanhuo1@126.com>
2024-02-01 10:20:22 +0800

    ✅ test(BaseConv): 增肌 Base32、Base64、Base91、Base256、ZBase32 单元测试

---
+++ tests/BingUtilsUT/ConvUT/BaseConvTest.cs
@@ -0,0 +1,148 @@
+﻿using System.Text;
+using Bing.Conversions;
+
+namespace BingUtilsUT.ConvUT;
+
+/// <summary>
+/// BASE 转换工具测试
+/// </summary>
+[Trait("ConvUT","BaseX")]
+public class BaseConvTest
+{
+    private const string TestValue = "Bing.Utils";
+
+    /// <summary>
+    /// 测试 - Base32 - 字符串
+    /// </summary>
+    [Fact]
+    public void Test_Base32String()
+    {
+        var baseVal = BaseConv.ToBase32String(TestValue);
+        var originalVal = BaseConv.FromBase32String(baseVal);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - Base32 - 字节数组
+    /// </summary>
+    [Fact]
+    public void Test_Base32Bytes()
+    {
+        var byteArray = Encoding.UTF8.GetBytes(TestValue);
+        var baseVal = BaseConv.ToBase32(byteArray);
+
+        var originalByteArray = BaseConv.FromBase32(baseVal);
+        var originalVal =  Encoding.UTF8.GetString(originalByteArray);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - Base64 - 字符串
+    /// </summary>
+    [Fact]
+    public void Test_Base64String()
+    {
+        var baseVal = BaseConv.ToBase64String(TestValue);
+        var originalVal = BaseConv.FromBase64String(baseVal);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - Base64 - 字节数组
+    /// </summary>
+    [Fact]
+    public void Test_Base64Bytes()
+    {
+        var byteArray = Encoding.UTF8.GetBytes(TestValue);
+        var baseVal = BaseConv.ToBase64(byteArray);
+
+        var originalByteArray = BaseConv.FromBase64(baseVal);
+        var originalVal =  Encoding.UTF8.GetString(originalByteArray);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - Base91 - 字符串
+    /// </summary>
+    [Fact]
+    public void Test_Base91String()
+    {
+        var baseVal = BaseConv.ToBase91String(TestValue);
+        var originalVal = BaseConv.FromBase91String(baseVal);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - Base91 - 字节数组
+    /// </summary>
+    [Fact]
+    public void Test_Base91Bytes()
+    {
+        var byteArray = Encoding.UTF8.GetBytes(TestValue);
+        var baseVal = BaseConv.ToBase91(byteArray);
+
+        var originalByteArray = BaseConv.FromBase91(baseVal);
+        var originalVal =  Encoding.UTF8.GetString(originalByteArray);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - Base256 - 字符串
+    /// </summary>
+    [Fact]
+    public void Test_Base256String()
+    {
+        var baseVal = BaseConv.ToBase256String(TestValue);
+        var originalVal = BaseConv.FromBase256String(baseVal);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - Base256 - 字节数组
+    /// </summary>
+    [Fact]
+    public void Test_Base256Bytes()
+    {
+        var byteArray = Encoding.UTF8.GetBytes(TestValue);
+        var baseVal = BaseConv.ToBase256(byteArray);
+
+        var originalByteArray = BaseConv.FromBase256(baseVal);
+        var originalVal =  Encoding.UTF8.GetString(originalByteArray);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - ZBase32 - 字符串
+    /// </summary>
+    [Fact]
+    public void Test_ZBase32String()
+    {
+        var baseVal = BaseConv.ToZBase32String(TestValue);
+        var originalVal = BaseConv.FromZBase32String(baseVal);
+
+        originalVal.ShouldBe(TestValue);
+    }
+
+    /// <summary>
+    /// 测试 - ZBase32 - 字节数组
+    /// </summary>
+    [Fact]
+    public void Test_ZBase32Bytes()
+    {
+        var byteArray = Encoding.UTF8.GetBytes(TestValue);
+        var baseVal = BaseConv.ToZBase32(byteArray);
+
+        var originalByteArray = BaseConv.FromZBase32(baseVal);
+        var originalVal =  Encoding.UTF8.GetString(originalByteArray);
+
+        originalVal.ShouldBe(TestValue);
+    }
+}
