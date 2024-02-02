e9ce7c7dde2cd6c6b807527553ba241f1d160b26
jian玄冰 <jianxuanhuo1@126.com>
2024-02-02 11:07:03 +0800

    ✅ test(Hex): 添加 Hex 单元测试

---
+++ tests/BingUtilsUT/ConvUT/HexTest.cs
@@ -0,0 +1,25 @@
+﻿using Bing.Conversions;
+
+namespace BingUtilsUT.ConvUT;
+
+/// <summary>
+/// 十六进制工具测试
+/// </summary>
+[Trait("ConvUT", "Hex")]
+public class HexTest
+{
+    /// <summary>
+    /// 测试 - 十六进制
+    /// </summary>
+    [Fact]
+    public void Test_Hex()
+    {
+        var dec = "1234567890";
+        var hex = AnyRadixConvert.X2X(dec, 10, 16); // Should be 499602D2
+
+        var hexBytes01 = Hex.ToBytes(hex); // Should be 73 150 2 210
+        var hex01 = Hex.ToString(hexBytes01);
+
+        hex01.ShouldBe(hex);
+    }
+}
