e3bdb9edea2aee6fe600e664239aaba233d65344
jian玄冰 <jianxuanhuo1@126.com>
2024-02-22 10:21:42 +0800

    ✨ feat(DesensitizedHelper): 新增 `DesensitizedHelper`脱敏帮助类

---
+++ src/Bing.Utils/Bing/Text/Extensions/String/Extensions.String.Mask.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+// ReSharper disable once CheckNamespace
+namespace Bing.Text;
+
+/// <summary>
+/// 字符串(<see cref="string"/>) 扩展
+/// </summary>
+public static partial class StringExtensions
+{
+    /// <summary>
+    /// 字符串掩码[俗称:脱敏]
+    /// </summary>
+    /// <param name="value">需要进行脱敏处理的字符串。</param>
+    /// <param name="mask">用于脱敏的掩码字符，默认为'*'。</param>
+    /// <returns>脱敏后的字符串。</returns>
+    /// <remarks>
+    /// 此方法根据字符串的长度，保留字符串的部分内容，其余部分使用指定的掩码字符替换。
+    /// - 如果字符串长度大于等于11，保留前3位和后4位，其余部分使用掩码字符替换。
+    /// - 如果字符串长度为10，保留前3位和后3位，其余部分使用掩码字符替换。
+    /// - 依此类推，长度递减时，保留的字符数量相应减少。
+    /// - 如果字符串长度小于或等于5，仅保留第一位，其余部分使用掩码字符替换。
+    /// 如果输入字符串为空或仅包含空白字符，则直接返回原字符串。
+    /// </remarks>
+    public static string Mask(this string value, char mask = '*')
+    {
+        if (string.IsNullOrWhiteSpace(value?.Trim()))
+            return value;
+        value = value.Trim();
+        var masks = mask.ToString().PadLeft(4, mask);
+        return value.Length switch
+        {
+            >= 11 => Regex.Replace(value, "(.{3}).*(.{4})", $"$1{masks}$2"),
+            10 => Regex.Replace(value, "(.{3}).*(.{3})", $"$1{masks}$2"),
+            9 => Regex.Replace(value, "(.{2}).*(.{3})", $"$1{masks}$2"),
+            8 => Regex.Replace(value, "(.{2}).*(.{2})", $"$1{masks}$2"),
+            7 => Regex.Replace(value, "(.{1}).*(.{2})", $"$1{masks}$2"),
+            6 => Regex.Replace(value, "(.{1}).*(.{1})", $"$1{masks}$2"),
+            _ => Regex.Replace(value, "(.{1}).*", $"$1{masks}")
+        };
+    }
+}
---
+++ tests/BingUtilsUT/StringUT/StringDesensitizedTest.cs
@@ -0,0 +1,111 @@
+﻿using Bing.Text;
+
+namespace BingUtilsUT.StringUT;
+
+[Trait("StringUT", "Strings.Desensitized")]
+public class StringDesensitizedTest
+{
+    /// <summary>
+    /// 测试 - 脱敏 - 中文名
+    /// </summary>
+    [Fact]
+    public void Test_ChineseName()
+    {
+        DesensitizedHelper.ChineseName("段正淳").ShouldBe("段**");
+    }
+
+    /// <summary>
+    /// 测试 - 脱敏 -身份证
+    /// </summary>
+    [Fact]
+    public void Test_IdCardNum()
+    {
+        DesensitizedHelper.IdCardNum("51343620000320711X",1,2).ShouldBe("5***************1X");
+    }
+
+    /// <summary>
+    /// 测试 - 脱敏 - 固定电话
+    /// </summary>
+    [Fact]
+    public void Test_FixedPhone()
+    {
+        DesensitizedHelper.FixedPhone("09157518479").ShouldBe("0915*****79");
+    }
+
+    /// <summary>
+    /// 测试 - 脱敏 - 手机号码
+    /// </summary>
+    [Fact]
+    public void Test_MobilePhone()
+    {
+        DesensitizedHelper.MobilePhone("13610000000").ShouldBe("136****0000");
+    }
+
+    /// <summary>
+    /// 测试 - 脱敏 - 地址
+    /// </summary>
+    [Fact]
+    public void Test_Address()
+    {
+        DesensitizedHelper.Address("广东省广州市天河区猎德街道289号",5).ShouldBe("广东省广州市天河区猎德街*****");
+        DesensitizedHelper.Address("广东省广州市天河区猎德街道289号",50).ShouldBe("*****************");
+        DesensitizedHelper.Address("广东省广州市天河区猎德街道289号",0).ShouldBe("广东省广州市天河区猎德街道289号");
+        DesensitizedHelper.Address("广东省广州市天河区猎德街道289号",-1).ShouldBe("广东省广州市天河区猎德街道289号");
+    }
+
+    /// <summary>
+    /// 测试 - 脱敏 - 邮箱
+    /// </summary>
+    [Fact]
+    public void Test_Email()
+    {
+        DesensitizedHelper.Email("wang@126.com").ShouldBe("w***@126.com");
+        DesensitizedHelper.Email("wang@gmail.com.cn").ShouldBe("w***@gmail.com.cn");
+        DesensitizedHelper.Email("wang-andy@gmail.com.cn").ShouldBe("w********@gmail.com.cn");
+    }
+
+    /// <summary>
+    /// 测试 - 脱敏 - 密码
+    /// </summary>
+    [Fact]
+    public void Test_Password()
+    {
+        DesensitizedHelper.Password("1234567890").ShouldBe("**********");
+    }
+
+    /// <summary>
+    /// 测试 - 脱敏 - 银行卡
+    /// </summary>
+    [Theory]
+    [InlineData(null,"")]
+    [InlineData("","")]
+    [InlineData(" ","")]
+    [InlineData("1234 2222 3333 4444 6789 9","1234 **** **** **** **** 9")]
+    [InlineData("1234 2222 3333 4444 6789 91","1234 **** **** **** **** 91")]
+    [InlineData("1234 2222 3333 4444 6789","1234 **** **** **** 6789")]
+    [InlineData("1234 2222 3333 4444 678","1234 **** **** **** 678")]
+    [InlineData("11011111222233333256","1101 **** **** **** 3256")]
+    [InlineData("6227880100100105123","6227 **** **** **** 123")]
+    public void Test_BankCard(string input, string result)
+    {
+        DesensitizedHelper.BankCard(input).ShouldBe(result);
+    }
+
+    /// <summary>
+    /// 测试 - 脱敏 - IPv4
+    /// </summary>
+    [Fact]
+    public void Test_IPv4()
+    {
+        DesensitizedHelper.IPv4("192.168.1.1").ShouldBe("192.*.*.*");
+    }
+    
+    /// <summary>
+    /// 测试 - 脱敏 - IPv6
+    /// </summary>
+    [Fact]
+    public void Test_IPv6()
+    {
+        DesensitizedHelper.IPv6("2001:0db8:86a3:08d3:1319:8a2e:0370:7344").ShouldBe("2001:*:*:*:*:*:*:*");
+    }
+}
