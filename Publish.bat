b1463528490d78053e92f5adc87e2364fad409a2
jian玄冰 <jianxuanhuo1@126.com>
2024-02-29 16:34:01 +0800

    🐳 chore(Publish): 调整发布脚本，增加 IdUtils 发布

--- Publish.bat
+++ Publish.bat
@@ -28,6 +28,7 @@ dotnet pack src/Bing.Utils.Drawing.SkiaSharp -c Release -o nuget_packages
 dotnet pack src/Bing.Utils.Http -c Release -o nuget_packages
 dotnet pack src/Bing.Utils.Collections -c Release -o nuget_packages
 dotnet pack src/Bing.Utils.Reflection -c Release -o nuget_packages
+dotnet pack src/Bing.Utils.IdUtils -c Release -o nuget_packages
 
 for /R "nuget_packages" %%s in (*symbols.nupkg) do (
     del %%s
