eac1339e3e89a89765c74d81a45d31aaac56c1a8
jian玄冰 <jianxuanhuo1@126.com>
2024-05-16 17:04:44 +0800

    ✅ test(Strings): 增加 `Strings.Get` 相关单元测试

---
+++ tests/BingUtilsUT/StringUT/StringsGettingTest.cs
@@ -0,0 +1,34 @@
+﻿using Bing.Text;
+
+namespace BingUtilsUT.StringUT;
+
+[Trait("StringUT", "Strings.Get")]
+public class StringsGettingTest
+{
+    /// <summary>
+    /// 测试 - 只获取字母和数字
+    /// </summary>
+    [Fact]
+    public void Test_GetNumbersAndLetters()
+    {
+        Strings.GetNumbersAndLetters("abcd1234").ShouldBe("abcd1234");
+    }
+
+    /// <summary>
+    /// 测试 - 只获取数字
+    /// </summary>
+    [Fact]
+    public void Test_GetNumbers()
+    {
+        Strings.GetNumbers("abcd1234").ShouldBe("1234");
+    }
+
+    /// <summary>
+    /// 测试 - 只获取字母
+    /// </summary>
+    [Fact]
+    public void Test_GetLetters()
+    {
+        Strings.GetLetters("abcd1234").ShouldBe("abcd");
+    }
+}
