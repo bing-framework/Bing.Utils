aa8cfbcb47a443328221c2fb7ad104dfb8200829
jian玄冰 <jianxuanhuo1@126.com>
2024-05-22 13:37:33 +0800

    🎨 refactor(Exception): 重构`Try.CreateAsync` 方法

--- src/Bing.Utils/Bing/Exceptions/Try.cs
+++ src/Bing.Utils/Bing/Exceptions/Try.cs
@@ -6,23 +6,6 @@
 public static partial class Try
 {
     /// <summary>
-    /// 创建
-    /// </summary>
-    /// <typeparam name="T">类型</typeparam>
-    /// <param name="createFuncAsync">创建函数</param>
-    public static Try<T> CreateFromTask<T>(Func<Task<T>> createFuncAsync)
-    {
-        try
-        {
-            return LiftValue(CallAsyncInSync(createFuncAsync));
-        }
-        catch (Exception e)
-        {
-            return LiftException<T>(e);
-        }
-    }
-
-    /// <summary>
     /// 提取值
     /// </summary>
     /// <typeparam name="T">类型</typeparam>
@@ -51,43 +34,5 @@ public static partial class Try
         if (task is null)
             throw new InvalidOperationException($"The task factory {nameof(taskFunc)} failed to run.");
         return ThenWaitAndUnwrapException(task, cancellationToken);
-    }
-
-    /// <summary>
-    /// 等待执行并展开异常
-    /// </summary>
-    /// <typeparam name="TResult">结果类型</typeparam>
-    /// <param name="task">异步</param>
-    /// <param name="cancellationToken">取消令牌</param>
-    private static TResult ThenWaitAndUnwrapException<TResult>(Task<TResult> task, CancellationToken cancellationToken)
-    {
-        if (task is null)
-            throw new ArgumentNullException(nameof(task));
-        try
-        {
-            task.Wait(cancellationToken);
-            return task.Result;
-        }
-        catch (AggregateException e)
-        {
-            throw ExceptionHelper.PrepareForRethrow(e.InnerException);
-        }
-    }
-
-    /// <summary>
-    /// 调用
-    /// </summary>
-    /// <param name="invokeAction">操作</param>
-    public static TryAction Invoke(Action invokeAction)
-    {
-        try
-        {
-            invokeAction();
-            return new SuccessAction(invokeAction.GetHashCode());
-        }
-        catch (Exception e)
-        {
-            return new FailureAction(e, invokeAction?.GetHashCode() ?? 0);
-        }
     }
 }
