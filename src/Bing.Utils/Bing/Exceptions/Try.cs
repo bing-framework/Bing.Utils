059256309021c5330ef6e567814229904e72b27f
jian玄冰 <jianxuanhuo1@126.com>
2024-05-22 15:23:09 +0800

    ✨ feat(Exception): 添加 `Try.InvokeRange` 方法

--- src/Bing.Utils/Bing/Exceptions/Try.cs
+++ src/Bing.Utils/Bing/Exceptions/Try.cs
@@ -19,20 +19,4 @@ public static partial class Try
     /// <param name="exception">异常</param>
     /// <param name="cause">导致错误的原因</param>
     public static Try<T> LiftException<T>(Exception exception, string cause = null) => new Failure<T>(exception, cause);
-
-    /// <summary>
-    /// 同步调异步
-    /// </summary>
-    /// <typeparam name="TResult">结果类型</typeparam>
-    /// <param name="taskFunc">异步函数</param>
-    /// <param name="cancellationToken">取消令牌</param>
-    private static TResult CallAsyncInSync<TResult>(Func<Task<TResult>> taskFunc, CancellationToken cancellationToken = default)
-    {
-        if (taskFunc is null)
-            throw new ArgumentNullException(nameof(taskFunc));
-        var task = Create(taskFunc).GetValue();
-        if (task is null)
-            throw new InvalidOperationException($"The task factory {nameof(taskFunc)} failed to run.");
-        return ThenWaitAndUnwrapException(task, cancellationToken);
-    }
 }
