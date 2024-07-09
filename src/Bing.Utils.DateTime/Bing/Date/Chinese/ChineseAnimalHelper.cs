3b12415d52a53baa9be1a744b88e58212ebfe4fc
jian玄冰 <jianxuanhuo1@126.com>
2024-07-09 14:12:06 +0800

    ✅ test(DateTime): 完善 中国生肖帮助类 单元测试

--- src/Bing.Utils.DateTime/Bing/Date/Chinese/ChineseAnimalHelper.cs
+++ src/Bing.Utils.DateTime/Bing/Date/Chinese/ChineseAnimalHelper.cs
@@ -21,25 +21,36 @@ public static class ChineseAnimalHelper
     private const int ANIMAL_START_YEAR = 1900;
 
     /// <summary>
-    /// 计算指定日期的生肖在生肖数组中的索引
+    /// 计算指定年份的生肖在生肖数组中的索引。
     /// </summary>
-    /// <param name="dt">需要计算生肖的日期</param>
-    /// <returns>生肖在生肖数组中的索引</returns>
-    private static int Index(DateTime dt)
+    /// <param name="year">年份。</param>
+    /// <returns>生肖在生肖数组中的索引。</returns>
+    private static int Index(int year)
     {
-        var offset = dt.Year - ANIMAL_START_YEAR;
+        var offset = year - ANIMAL_START_YEAR;
         return offset % 12;
     }
 
     /// <summary>
-    /// 获取指定日期的生肖
+    /// 获取指定日期的生肖。
     /// </summary>
-    /// <param name="dt">需要获取生肖的日期</param>
-    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为 false，即使用简体中文字符</param>
-    /// <returns>指定日期的生肖</returns>
-    public static string Get(DateTime dt, bool traditionalChineseCharacters = false)
+    /// <param name="dt">需要获取生肖的日期。</param>
+    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为 false，即使用简体中文字符。</param>
+    /// <returns>指定日期的生肖。</returns>
+    public static string Get(DateTime dt, bool traditionalChineseCharacters = false) => Get(dt.Year, traditionalChineseCharacters);
+
+    /// <summary>
+    /// 获取指定年份的生肖。
+    /// </summary>
+    /// <param name="year">需要获取生肖的年份</param>
+    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为 false，即使用简体中文字符。</param>
+    /// <returns>指定年份的生肖。</returns>
+    /// <exception cref="ArgumentOutOfRangeException">当年份小于1900时抛出此异常。</exception>
+    public static string Get(int year, bool traditionalChineseCharacters = false)
     {
+        if (year < ANIMAL_START_YEAR)
+            throw new ArgumentOutOfRangeException(nameof(year), $"年份不能小于{ANIMAL_START_YEAR}。");
         var animal = traditionalChineseCharacters ? ANIMAL_Z : ANIMAL_S;
-        return animal[Index(dt)];
+        return animal[Index(year)];
     }
 }
