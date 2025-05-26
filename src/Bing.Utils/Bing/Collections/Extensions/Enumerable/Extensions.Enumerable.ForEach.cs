8b174d78185d2bc905d3c8d0309834968920ca37
jian玄冰 <jianxuanhuo1@126.com>
2025-05-26 18:22:52 +0800

    🎨 refactor(Enumerable): 优化 ForEach 扩展方法并添加并行处理支持
    - 重写 ForEach 扩展方法，使用 foreach 循环替代数组操作
    - 添加 ForEachAsync 扩展方法的重载版本，支持指定最大并行度
    - 新增 ParallelForEachAsync 私有方法，用于实现并行处理逻辑
    - 优化异常处理和参数验证

--- src/Bing.Utils/Bing/Collections/Extensions/Enumerable/Extensions.Enumerable.ForEach.cs
+++ src/Bing.Utils/Bing/Collections/Extensions/Enumerable/Extensions.Enumerable.ForEach.cs
@@ -36,9 +36,10 @@ public static partial class EnumerableExtensions
             throw new ArgumentNullException(nameof(enumerable), $@"源{typeof(T).Name}集合对象不可为空！");
         if (action == null)
             throw new ArgumentNullException(nameof(action), @"操作表达式不可为空！");
-        var array = enumerable.ToArray();
-        for (var i = 0; i < array.Length; i++) 
-            action(array[i], i);
+
+        var index = 0;
+        foreach (var item in enumerable)
+            action(item, index++);
     }
 
     /// <summary>
@@ -47,13 +48,48 @@ public static partial class EnumerableExtensions
     /// <typeparam name="T">类型</typeparam>
     /// <param name="enumerable">值</param>
     /// <param name="action">操作</param>
+    /// <param name="maxDegreeOfParallelism">最大并行度，默认为无限制</param>
     /// <exception cref="ArgumentNullException">源集合对象为空、操作表达式为空</exception>
-    public static Task ForEachAsync<T>(this IEnumerable<T> enumerable, Func<T, Task> action)
+    public static Task ForEachAsync<T>(this IEnumerable<T> enumerable, Func<T, Task> action, int? maxDegreeOfParallelism = null)
     {
         if (enumerable == null)
             throw new ArgumentNullException(nameof(enumerable), $@"源{typeof(T).Name}集合对象不可为空！");
         if (action == null)
             throw new ArgumentNullException(nameof(action), @"操作表达式不可为空！");
-        return Task.WhenAll(from item in enumerable select Task.Run(() => action(item)));
+        // 如果未指定并行度，直接并行执行所有任务
+        if (!maxDegreeOfParallelism.HasValue)
+            return Task.WhenAll(enumerable.Select(action));
+        // 有并行度限制时，使用 SemaphoreSlim 控制并行度
+        return ParallelForEachAsync(enumerable, action, maxDegreeOfParallelism.Value);
+    }
+
+    /// <summary>
+    /// 对指定集合中的每个元素执行指定操作
+    /// </summary>
+    /// <typeparam name="T">类型</typeparam>
+    /// <param name="source">源</param>
+    /// <param name="action">操作</param>
+    /// <param name="maxDegreeOfParallelism">最大并行度</param>
+    /// <returns></returns>
+    private static async Task ParallelForEachAsync<T>(IEnumerable<T> source, Func<T, Task> action, int maxDegreeOfParallelism)
+    {
+        var semaphore = new SemaphoreSlim(maxDegreeOfParallelism, maxDegreeOfParallelism);
+        var tasks = new List<Task>();
+        foreach (var item in source)
+        {
+            await semaphore.WaitAsync();
+            tasks.Add(Task.Run(async () =>
+            {
+                try
+                {
+                    await action(item);
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }));
+        }
+        await Task.WhenAll(tasks);
     }
 }
