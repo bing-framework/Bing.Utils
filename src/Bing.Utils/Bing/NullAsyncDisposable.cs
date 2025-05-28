91de1f35006129ce9f5e90b6492b92fae4c089de
jian玄冰 <jianxuanhuo1@126.com>
2025-05-28 17:30:15 +0800

    📝 docs: 更新类的 XML 注释并添加使用示例
    - 为 DisposeAction、NameValue、NullDisposable 类添加详细的 XML 注释
    - 添加 DisposeAction 和 NameValue 类的使用示例
    - 优化 AsyncHelper 类中方法的 XML 注释
    - 简化 AsyncLock 类中 Releaser 结构的注释

---
+++ src/Bing.Utils/Bing/NullAsyncDisposable.cs
@@ -0,0 +1,29 @@
+﻿namespace Bing;
+
+/// <summary>
+/// 表示一个空的异步可释放对象，用于实现 <see cref="IAsyncDisposable"/> 接口但不执行任何实际的资源释放操作。
+/// 这是 <see cref="NullDisposable"/> 的异步版本，遵循相同的设计模式。
+/// </summary>
+public sealed class NullAsyncDisposable : IAsyncDisposable
+{
+    /// <summary>
+    /// 获取 <see cref="NullAsyncDisposable"/> 类的单例实例。
+    /// 所有代码都应该使用此共享实例而不是创建新实例。
+    /// </summary>
+    public static NullAsyncDisposable Instance { get; } = new();
+
+    /// <summary>
+    /// 初始化一个<see cref="NullAsyncDisposable"/>类型的实例
+    /// </summary>
+    private NullAsyncDisposable() { }
+
+    /// <summary>
+    /// 实现 <see cref="IAsyncDisposable.DisposeAsync"/> 方法，但不执行任何资源释放操作。
+    /// </summary>
+    /// <returns>表示异步释放操作的 <see cref="ValueTask"/>，此任务已完成且不包含任何工作。</returns>
+    /// <remarks>
+    /// 此方法总是返回 <c>default</c> 值的 <see cref="ValueTask"/>，这表示一个已完成的无值任务，
+    /// 不会分配新的对象，符合高性能的资源管理要求。
+    /// </remarks>
+    public ValueTask DisposeAsync() => default;
+}
--- src/Bing.Utils/Bing/NullDisposable.cs
+++ src/Bing.Utils/Bing/NullDisposable.cs
@@ -1,14 +1,15 @@
 ﻿namespace Bing;
 
 /// <summary>
-/// 空释放
+/// 表示一个空的可释放对象，用于实现 <see cref="IDisposable"/> 接口但不执行任何实际的资源释放操作。
 /// </summary>
 public sealed class NullDisposable : IDisposable
 {
     /// <summary>
-    /// 实例
+    /// 获取 <see cref="NullDisposable"/> 类的单例实例。
+    /// 所有代码都应该使用此共享实例而不是创建新实例。
     /// </summary>
-    public static NullDisposable Instance { get; } = new NullDisposable();
+    public static NullDisposable Instance { get; } = new();
 
     /// <summary>
     /// 初始化一个<see cref="NullDisposable"/>类型的实例
@@ -16,7 +17,11 @@ public sealed class NullDisposable : IDisposable
     private NullDisposable() { }
 
     /// <summary>
-    /// 释放资源
+    /// 实现 <see cref="IDisposable.Dispose"/> 方法，但不执行任何资源释放操作。
     /// </summary>
+    /// <remarks>
+    /// 此方法是故意留空的，因为 NullDisposable 的设计目的就是提供一个不执行任何操作的 Dispose 方法。
+    /// 当需要满足 IDisposable 接口约定但实际上没有资源需要释放时，可以使用此类。
+    /// </remarks>
     public void Dispose() { }
 }
