7c1b0aada6b852d23b163825e94262ca701bcec5
jian玄冰 <jianxuanhuo1@126.com>
2025-06-23 10:43:56 +0800

    ✨ feat(Threading): 添加 Delegate 扩展方法及测试
    - 新增 DelegateExtensions 类，提供委托的安全调用和重试机制
    - 添加 InvokeSafe、Try、InvokeOrDefault 等扩展方法
    - 新增 DelegateExtensionsTest 类，对扩展方法进行单元测试
    - 测试覆盖非空委托、空委托、成功执行、失败执行、重试机制等场景

---
+++ src/Bing.Utils/Bing/Threading/Extensions/DelegateExtensions.cs
@@ -0,0 +1,125 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Bing.Threading;
+
+/// <summary>
+/// 委托(<see cref="Delegate"/>) 扩展
+/// </summary>
+public static class DelegateExtensions
+{
+    /// <summary>
+    /// 安全调用委托
+    /// </summary>
+    /// <param name="del">委托</param>
+    /// <param name="parameters">参数</param>
+    /// <remarks>
+    /// 如果委托为 null，则不执行任何操作
+    /// </remarks>
+    public static void InvokeSafe(this Delegate del, params object[] parameters)
+    {
+        del?.DynamicInvoke(parameters);
+    }
+
+    /// <summary>
+    /// 尝试执行委托
+    /// </summary>
+    /// <param name="del">委托</param>
+    /// <param name="parameters">参数</param>
+    /// <returns>执行成功返回 true，失败返回 false</returns>
+    /// <remarks>
+    /// 此方法会捕获执行过程中的异常，并尝试执行一次
+    /// </remarks>
+    public static bool Try(this Delegate del, params object[] parameters) => Try(del, 1, TimeSpan.Zero, parameters);
+
+    /// <summary>
+    /// 尝试执行委托
+    /// </summary>
+    /// <param name="del">委托</param>
+    /// <param name="maxTries">最大尝试次数，如果设为0或负值则表示无限尝试</param>
+    /// <param name="interval">尝试间隔时间</param>
+    /// <param name="parameters">参数</param>
+    /// <returns>执行成功返回 true，失败返回 false</returns>
+    /// <remarks>
+    /// 此方法会捕获执行过程中的异常，并根据指定的次数和间隔重试
+    /// </remarks>
+    public static bool Try(this Delegate del, int maxTries, TimeSpan interval, params object[] parameters)
+    {
+        var success = false;
+        var triesCount = 0;
+        while (!success)
+        {
+            try
+            {
+                del.DynamicInvoke(parameters);
+                success = true;
+            }
+            catch
+            {
+                triesCount++;
+                // 如果达到最大尝试次数，则退出循环
+                if (maxTries > 0 && triesCount >= maxTries)
+                    break;
+
+                if (interval > TimeSpan.Zero)
+                    Thread.Sleep(interval);
+            }
+        }
+        return success;
+    }
+
+    /// <summary>
+    /// 调用委托并返回结果，如果执行失败则返回默认值
+    /// </summary>
+    /// <typeparam name="T">返回值类型</typeparam>
+    /// <param name="del">委托</param>
+    /// <param name="defaultValue">默认值，执行失败时返回</param>
+    /// <param name="parameters">参数</param>
+    /// <returns>执行成功返回结果，失败返回默认值</returns>
+    /// <remarks>
+    /// 此方法会捕获执行过程中的异常，并尝试执行一次
+    /// </remarks>
+    public static T InvokeOrDefault<T>(this Delegate del, T defaultValue = default, params object[] parameters) =>
+        InvokeOrDefault(del, 1, TimeSpan.Zero, defaultValue, parameters);
+
+    /// <summary>
+    /// 调用委托并返回结果，如果执行失败则返回默认值
+    /// </summary>
+    /// <typeparam name="T">返回值类型</typeparam>
+    /// <param name="del">委托</param>
+    /// <param name="maxTries">最大尝试次数，如果设为0或负值则表示无限尝试</param>
+    /// <param name="interval">尝试间隔时间</param>
+    /// <param name="defaultValue">默认值，执行失败时返回</param>
+    /// <param name="parameters">参数</param>
+    /// <returns>执行成功返回结果，失败返回默认值</returns>
+    /// <remarks>
+    /// 此方法会捕获执行过程中的异常，并根据指定的次数和间隔重试
+    /// </remarks>
+    public static T InvokeOrDefault<T>(this Delegate del, int maxTries, TimeSpan interval, T defaultValue = default, params object[] parameters)
+    {
+        var result = defaultValue;
+
+        var triesCount = 0;
+        while (true)
+        {
+            try
+            {
+                var obj = del.DynamicInvoke(parameters);
+                if (obj is T o)
+                    result = o;
+                else
+                    result = defaultValue;
+                break;
+            }
+            catch
+            {
+                triesCount++;
+                // 如果达到最大尝试次数，则返回默认值
+                if (maxTries > 0 && triesCount >= maxTries)
+                    break;
+
+                if (interval > TimeSpan.Zero)
+                    Thread.Sleep(interval);
+            }
+        }
+        return result;
+    }
+}
---
+++ tests/Bing.Utils.Tests/Bing/Threading/DelegateExtensionsTest.cs
@@ -0,0 +1,214 @@
+﻿namespace Bing.Threading;
+
+/// <summary>
+/// 委托(<see cref="Delegate"/>) 扩展 测试
+/// </summary>
+public class DelegateExtensionsTest
+{
+    /// <summary>
+    /// 测试 - 安全调用委托 - 非空委托
+    /// </summary>
+    [Fact]
+    public void Test_InvokeSafe_NonNullDelegate()
+    {
+        // Arrange
+        int result = 0;
+        Action<int> action = x => result = x;
+
+        // Act
+        action.InvokeSafe(5);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+
+    /// <summary>
+    /// 测试 - 安全调用委托 - 空委托
+    /// </summary>
+    [Fact]
+    public void Test_InvokeSafe_NullDelegate()
+    {
+        // Arrange
+        Action action = null;
+
+        // Act & Assert
+        // 确保不会抛出异常
+        action.InvokeSafe();
+    }
+
+    /// <summary>
+    /// 测试 - 尝试执行委托 - 成功情况
+    /// </summary>
+    [Fact]
+    public void Test_Try_Success()
+    {
+        // Arrange
+        int result = 0;
+        Action<int> action = x => result = x;
+
+        // Act
+        var success = action.Try(5);
+
+        // Assert
+        Assert.True(success);
+        Assert.Equal(5, result);
+    }
+
+    /// <summary>
+    /// 测试 - 尝试执行委托 - 失败情况
+    /// </summary>
+    [Fact]
+    public void Test_Try_Failure()
+    {
+        // Arrange
+        Action action = () => throw new Exception("Test exception");
+
+        // Act
+        var success = action.Try();
+
+        // Assert
+        Assert.False(success);
+    }
+
+    /// <summary>
+    /// 测试 - 尝试执行委托 - 重试情况
+    /// </summary>
+    [Fact]
+    public void Test_Try_WithRetries()
+    {
+        // Arrange
+        int callCount = 0;
+        Action action = () =>
+        {
+            callCount++;
+            if (callCount < 3)
+                throw new Exception("Test exception");
+        };
+
+        // Act
+        var success = action.Try(3, TimeSpan.FromMilliseconds(10));
+
+        // Assert
+        Assert.True(success);
+        Assert.Equal(3, callCount);
+    }
+
+    /// <summary>
+    /// 测试 - 尝试执行委托 - 达到最大重试次数仍失败
+    /// </summary>
+    [Fact]
+    public void Test_Try_ExceedMaxRetries()
+    {
+        // Arrange
+        int callCount = 0;
+        Action action = () =>
+        {
+            callCount++;
+            throw new Exception("Test exception");
+        };
+
+        // Act
+        var success = action.Try(3, TimeSpan.FromMilliseconds(10));
+
+        // Assert
+        Assert.False(success);
+        Assert.Equal(3, callCount);
+    }
+
+    /// <summary>
+    /// 测试 - 调用委托并返回结果 - 成功情况
+    /// </summary>
+    [Fact]
+    public void Test_InvokeOrDefault_Success()
+    {
+        // Arrange
+        Func<int, int> func = x => x * 2;
+
+        // Act
+        var result = func.InvokeOrDefault(0, 5);
+
+        // Assert
+        Assert.Equal(10, result);
+    }
+
+    /// <summary>
+    /// 测试 - 调用委托并返回结果 - 失败情况
+    /// </summary>
+    [Fact]
+    public void Test_InvokeOrDefault_Failure()
+    {
+        // Arrange
+        Func<int> func = () => throw new Exception("Test exception");
+
+        // Act
+        var result = func.InvokeOrDefault(42);
+
+        // Assert
+        Assert.Equal(42, result);
+    }
+
+    /// <summary>
+    /// 测试 - 调用委托并返回结果 - 类型不匹配情况
+    /// </summary>
+    [Fact]
+    public void Test_InvokeOrDefault_TypeMismatch()
+    {
+        // Arrange
+        Delegate del = new Func<string>(() => "test");
+        int defaultValue = 42;
+
+        // Act
+        // 使用 InvokeOrDefault<int> 尝试将 string 转换为 int，应该返回默认值
+        var result = del.InvokeOrDefault(defaultValue);
+
+        // Assert
+        Assert.Equal(defaultValue, result);
+    }
+
+    /// <summary>
+    /// 测试 - 调用委托并返回结果 - 重试情况
+    /// </summary>
+    [Fact]
+    public void Test_InvokeOrDefault_WithRetries()
+    {
+        // Arrange
+        int callCount = 0;
+        Func<int> func = () =>
+        {
+            callCount++;
+            if (callCount < 3)
+                throw new Exception("Test exception");
+            return 99;
+        };
+
+        // Act
+        var result = func.InvokeOrDefault(3, TimeSpan.FromMilliseconds(10), 42);
+
+        // Assert
+        Assert.Equal(99, result);
+        Assert.Equal(3, callCount);
+    }
+
+    /// <summary>
+    /// 测试 - 调用委托并返回结果 - 达到最大重试次数仍失败
+    /// </summary>
+    [Fact]
+    public void Test_InvokeOrDefault_ExceedMaxRetries()
+    {
+        // Arrange
+        int callCount = 0;
+        Func<int> func = () =>
+        {
+            callCount++;
+            throw new Exception("Test exception");
+        };
+        int defaultValue = 42;
+
+        // Act
+        var result = func.InvokeOrDefault(3, TimeSpan.FromMilliseconds(10), defaultValue);
+
+        // Assert
+        Assert.Equal(defaultValue, result);
+        Assert.Equal(3, callCount);
+    }
+}
