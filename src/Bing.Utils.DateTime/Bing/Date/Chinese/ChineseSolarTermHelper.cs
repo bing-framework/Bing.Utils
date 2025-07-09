905aa245c83d5d845ab420e84c0578cb560c6b96
jian玄冰 <jianxuanhuo1@126.com>
2025-07-09 10:40:19 +0800

    🎨 refactor(DateTime): 重构节气计算逻辑
    - 移除了不必要的 using 指令
    - 添加了基准日期时间和每年分钟偏移量的常量
    - 优化了节气计算方法，提高了代码可读性和性能
    - 增加了对无效输入的异常处理
    - 修正了部分方法的返回值和参数默认值

--- src/Bing.Utils.DateTime/Bing/Date/Chinese/ChineseSolarTermHelper.cs
+++ src/Bing.Utils.DateTime/Bing/Date/Chinese/ChineseSolarTermHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System.Globalization;
-using System.Linq.Expressions;
 
 namespace Bing.Date.Chinese;
 
@@ -8,6 +7,16 @@ namespace Bing.Date.Chinese;
 /// </summary>
 public static class ChineseSolarTermHelper
 {
+    /// <summary>
+    /// 基准日期时间：1900年1月6日2:05:00
+    /// </summary>
+    private static readonly DateTime BaseDateAndTime = new(1900, 1, 6, 2, 5, 0);
+
+    /// <summary>
+    /// 每年的分钟偏移量
+    /// </summary>
+    private const double MinutesPerYear = 525948.76;
+
     /// <summary>
     /// 24节气 - 简体
     /// </summary>
@@ -73,7 +82,7 @@ public static class ChineseSolarTermHelper
     ];
 
     /// <summary>
-    /// 24节气信息
+    /// 24节气信息，保存每个节气的分钟偏移量
     /// </summary>
     private static readonly int[] SOLAR_TERM_INFO =
     [
@@ -85,11 +94,14 @@ public static class ChineseSolarTermHelper
     /// 获取指定节气的中文名称
     /// </summary>
     /// <param name="solarTerms">节气枚举</param>
-    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为false，使用简体中文</param>
+    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符</param>
     /// <returns>返回指定节气的中文名称</returns>
+    /// <exception cref="ArgumentOutOfRangeException">节气枚举不在有效范围内时抛出</exception>
     public static string GetName(ChineseSolarTerms solarTerms, bool traditionalChineseCharacters = false)
     {
         var index = SOLAR_TERM_ENUM.IndexOf(solarTerms);
+        if (index == -1)
+            throw new ArgumentOutOfRangeException(nameof(solarTerms), "无效的节气枚举值");
         var solarTermP = traditionalChineseCharacters ? SOLAR_TERM_Z : SOLAR_TERM_S;
         return solarTermP[index];
     }
@@ -99,9 +111,12 @@ public static class ChineseSolarTermHelper
     /// </summary>
     /// <param name="solarTerms">节气枚举</param>
     /// <returns>返回指定节气的英文名称</returns>
+    /// <exception cref="ArgumentOutOfRangeException">节气枚举不在有效范围内时抛出</exception>
     public static string GetEnglishName(ChineseSolarTerms solarTerms)
     {
         var index = SOLAR_TERM_ENUM.IndexOf(solarTerms);
+        if (index == -1)
+            throw new ArgumentOutOfRangeException(nameof(solarTerms), "无效的节气枚举值");
         return SOLAR_TERM_E[index];
     }
 
@@ -110,13 +125,16 @@ public static class ChineseSolarTermHelper
     /// </summary>
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
-    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为false，使用简体中文</param>
+    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符</param>
     /// <returns>如果指定日期是节气，则返回节气的名称；否则返回空字符串</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static string GetSolarTerm(ChineseLunisolarCalendar calendar, DateTime targetDt, bool traditionalChineseCharacters = false)
     {
-        var i = SolarTermFunc(calendar, targetDt, (x, y) => x == y, out _);
+        if (calendar == null)
+            throw new ArgumentNullException(nameof(calendar), "中国农历日历不能为空");
+        var i = GetSolarTermIndex(calendar, targetDt, (x, y) => Math.Abs(x - y) <= 1, out _);
         var solarTermP = traditionalChineseCharacters ? SOLAR_TERM_Z : SOLAR_TERM_S;
-        return i == -1 ? "" : solarTermP[i];
+        return i == -1 ? string.Empty : solarTermP[i];
     }
 
     /// <summary>
@@ -124,8 +142,9 @@ public static class ChineseSolarTermHelper
     /// </summary>
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
-    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为false，使用简体中文</param>
+    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符</param>
     /// <returns>如果找到符合条件的节气，则返回节气的名称；否则返回空字符串</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static string GetLastSolarTerm(ChineseLunisolarCalendar calendar, DateTime targetDt, bool traditionalChineseCharacters = false) => GetLastSolarTerm(calendar, targetDt, out _, traditionalChineseCharacters);
 
     /// <summary>
@@ -134,13 +153,16 @@ public static class ChineseSolarTermHelper
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
     /// <param name="dt">计算得到的符合条件的节气日期</param>
-    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为false，使用简体中文</param>
+    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符</param>
     /// <returns>如果找到符合条件的节气，则返回节气的名称；否则返回空字符串</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static string GetLastSolarTerm(ChineseLunisolarCalendar calendar, DateTime targetDt, out DateTime dt, bool traditionalChineseCharacters = false)
     {
-        var i = SolarTermFunc(calendar, targetDt, (x, y) => x < y, out dt);
+        if (calendar == null)
+            throw new ArgumentNullException(nameof(calendar), "中国农历日历不能为空");
+        var i = GetSolarTermIndex(calendar, targetDt, (x, y) => x < y, out dt);
         var solarTermP = traditionalChineseCharacters ? SOLAR_TERM_Z : SOLAR_TERM_S;
-        return i == -1 ? "" : solarTermP[i];
+        return i == -1 ? string.Empty : solarTermP[i];
     }
 
     /// <summary>
@@ -148,8 +170,9 @@ public static class ChineseSolarTermHelper
     /// </summary>
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
-    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为false，使用简体中文</param>
+    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符</param>
     /// <returns>如果找到符合条件的节气，则返回节气的名称；否则返回空字符串</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static string GetNextSolarTerm(ChineseLunisolarCalendar calendar, DateTime targetDt, bool traditionalChineseCharacters = false) => GetNextSolarTerm(calendar, targetDt, out _, traditionalChineseCharacters);
 
     /// <summary>
@@ -158,13 +181,16 @@ public static class ChineseSolarTermHelper
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
     /// <param name="dt">计算得到的符合条件的节气日期</param>
-    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符，默认为false，使用简体中文</param>
+    /// <param name="traditionalChineseCharacters">是否使用繁体中文字符</param>
     /// <returns>如果找到符合条件的节气，则返回节气的名称；否则返回空字符串</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static string GetNextSolarTerm(ChineseLunisolarCalendar calendar, DateTime targetDt, out DateTime dt, bool traditionalChineseCharacters = false)
     {
-        var i = SolarTermFunc(calendar, targetDt, (x, y) => x > y, out dt);
+        if (calendar == null)
+            throw new ArgumentNullException(nameof(calendar), "中国农历日历不能为空");
+        var i = GetSolarTermIndex(calendar, targetDt, (x, y) => x > y, out dt);
         var solarTermP = traditionalChineseCharacters ? SOLAR_TERM_Z : SOLAR_TERM_S;
-        return i == -1 ? "" : solarTermP[i];
+        return i == -1 ? string.Empty : solarTermP[i];
     }
 
     /// <summary>
@@ -173,9 +199,12 @@ public static class ChineseSolarTermHelper
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
     /// <returns>如果指定日期是节气，则返回节气的枚举值；否则返回null</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static ChineseSolarTerms? GetSolarTermEnum(ChineseLunisolarCalendar calendar, DateTime targetDt)
     {
-        var i = SolarTermFunc(calendar, targetDt, (x, y) => x == y, out _);
+        if (calendar == null)
+            throw new ArgumentNullException(nameof(calendar), "中国农历日历不能为空");
+        var i = GetSolarTermIndex(calendar, targetDt, (x, y) => Math.Abs(x - y) <= 1, out _);
         return i == -1 ? null : SOLAR_TERM_ENUM[i];
     }
 
@@ -185,6 +214,7 @@ public static class ChineseSolarTermHelper
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
     /// <returns>如果找到符合条件的节气，则返回节气的枚举值；否则返回null</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static ChineseSolarTerms? GetLastSolarTermEnum(ChineseLunisolarCalendar calendar, DateTime targetDt) => GetLastSolarTermEnum(calendar, targetDt, out _);
 
     /// <summary>
@@ -194,9 +224,12 @@ public static class ChineseSolarTermHelper
     /// <param name="targetDt">目标日期</param>
     /// <param name="dt">计算得到的符合条件的节气日期</param>
     /// <returns>如果找到符合条件的节气，则返回节气的枚举值；否则返回null</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static ChineseSolarTerms? GetLastSolarTermEnum(ChineseLunisolarCalendar calendar, DateTime targetDt, out DateTime dt)
     {
-        var i = SolarTermFunc(calendar, targetDt, (x, y) => x < y, out dt);
+        if (calendar == null)
+            throw new ArgumentNullException(nameof(calendar), "中国农历日历不能为空");
+        var i = GetSolarTermIndex(calendar, targetDt, (x, y) => x < y, out dt);
         return i == -1 ? null : SOLAR_TERM_ENUM[i];
     }
 
@@ -206,6 +239,7 @@ public static class ChineseSolarTermHelper
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
     /// <returns>如果找到符合条件的节气，则返回节气的枚举值；否则返回null</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static ChineseSolarTerms? GetNextSolarTermEnum(ChineseLunisolarCalendar calendar, DateTime targetDt) => GetNextSolarTermEnum(calendar, targetDt, out _);
 
     /// <summary>
@@ -215,43 +249,51 @@ public static class ChineseSolarTermHelper
     /// <param name="targetDt">目标日期</param>
     /// <param name="dt">计算得到的符合条件的节气日期</param>
     /// <returns>如果找到符合条件的节气，则返回节气的枚举值；否则返回null</returns>
+    /// <exception cref="ArgumentNullException">日历为空时抛出</exception>
     public static ChineseSolarTerms? GetNextSolarTermEnum(ChineseLunisolarCalendar calendar, DateTime targetDt, out DateTime dt)
     {
-        var i = SolarTermFunc(calendar, targetDt, (x, y) => x > y, out dt);
+        if (calendar == null)
+            throw new ArgumentNullException(nameof(calendar), "中国农历日历不能为空");
+        var i = GetSolarTermIndex(calendar, targetDt, (x, y) => x > y, out dt);
         return i == -1 ? null : SOLAR_TERM_ENUM[i];
     }
 
     /// <summary>
-    /// 节气计算（当前年），返回指定条件的节气序及日期（公历）
+    /// 节气计算，返回指定条件的节气序及日期（公历）
     /// </summary>
     /// <param name="calendar">中国农历日历实例</param>
     /// <param name="targetDt">目标日期</param>
-    /// <param name="func">条件表达式，用于确定如何比较日期</param>
+    /// <param name="comparer">用于比较日期的函数</param>
     /// <param name="dateTime">计算得到的符合条件的节气日期</param>
     /// <returns>返回节气的索引，如果未找到符合条件的节气，则返回-1</returns>
-    private static int SolarTermFunc(ChineseLunisolarCalendar calendar, DateTime targetDt, Expression<Func<int, int, bool>> func, out DateTime dateTime)
+    private static int GetSolarTermIndex(ChineseLunisolarCalendar calendar, DateTime targetDt, Func<int, int, bool> comparer, out DateTime dateTime)
     {
-        var baseDateAndTime = new DateTime(1900, 1, 6, 2, 5, 0); //#1900-01-06 2:05:00 AM#
-        var year = targetDt.Year;
-        int[] solar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-        var expressionType = func.Body.NodeType;
-        if (expressionType != ExpressionType.LessThan && expressionType != ExpressionType.LessThanOrEqual &&
-            expressionType != ExpressionType.GreaterThan && expressionType != ExpressionType.GreaterThanOrEqual &&
-            expressionType != ExpressionType.Equal)
-            throw new NotSupportedException("不受支持的操作符。");
-        if (expressionType == ExpressionType.LessThan || expressionType == ExpressionType.LessThanOrEqual)
-            solar = solar.OrderByDescending(x => x).ToArray();
-        foreach (var item in solar)
+        if (calendar == null)
+            throw new ArgumentNullException(nameof(calendar), "中国农历日历不能为空");
+        if (targetDt < BaseDateAndTime || targetDt > new DateTime(2100, 12, 31))
         {
-            var num = 525948.76 * (year - 1900) + SOLAR_TERM_INFO[item - 1];
-            var newDate = baseDateAndTime.AddMinutes(num); // 按分钟计算
-            if (func.Compile()(newDate.DayOfYear, targetDt.DayOfYear))
-            {
-                dateTime = newDate;
-                return item - 1;
-            }
+            dateTime = calendar.MinSupportedDateTime;
+            return -1;
         }
 
+        var year = targetDt.Year;
+        // 获取24节气的序号列表，根据比较函数决定顺序
+        var solarTermIndices = Enumerable.Range(1, 24).ToArray();
+        if (comparer(0, 1)) // 降序
+            solarTermIndices = solarTermIndices.OrderByDescending(x => x).ToArray();
+        foreach (var termIndex in solarTermIndices)
+        {
+            // 计算节气对应的分钟数
+            var minutes = MinutesPerYear * (year - 1900) + SOLAR_TERM_INFO[termIndex - 1];
+            // 计算节气对应的日期
+            var termDate = BaseDateAndTime.AddMinutes(minutes);
+            // 根据比较函数判断是否符合条件
+            if (comparer(termDate.DayOfYear, targetDt.DayOfYear))
+            {
+                dateTime = termDate;
+                return termIndex - 1;
+            }
+        }
         dateTime = calendar.MinSupportedDateTime;
         return -1;
     }
---
+++ tests/Bing.Utils.DateTime.Tests/Bing/Date/Chinese/ChineseSolarTermHelperTest.cs
@@ -0,0 +1,210 @@
+﻿using System.Globalization;
+
+namespace Bing.Date.Chinese;
+
+/// <summary>
+/// 中国二十四节气帮助类测试
+/// </summary>
+[Trait("DateTimeUT", "ChineseDate.SolarTerm")]
+public class ChineseSolarTermHelperTest
+{
+    /// <summary>
+    /// 中国农历日历实例
+    /// </summary>
+    private readonly ChineseLunisolarCalendar _calendar = new();
+
+    /// <summary>
+    /// 测试 - GetName - 获取节气的中文名称
+    /// </summary>
+    [Theory]
+    [InlineData(ChineseSolarTerms.BeginningOfSpring, false, "立春")]
+    [InlineData(ChineseSolarTerms.VernalEquinox, false, "春分")]
+    [InlineData(ChineseSolarTerms.QingmingFestival, true, "清明")]
+    [InlineData(ChineseSolarTerms.TheWakingOfInsects, true, "驚蟄")]
+    public void GetName_ValidTerms_ReturnsCorrectName(ChineseSolarTerms term, bool traditional, string expected)
+    {
+        // Act
+        var result = ChineseSolarTermHelper.GetName(term, traditional);
+
+        // Assert
+        result.ShouldBe(expected);
+    }
+
+    /// <summary>
+    /// 测试 - GetEnglishName - 获取节气的英文名称
+    /// </summary>
+    [Theory]
+    [InlineData(ChineseSolarTerms.BeginningOfSpring, "Beginning of Spring")]
+    [InlineData(ChineseSolarTerms.VernalEquinox, "Vernal Equinox")]
+    [InlineData(ChineseSolarTerms.QingmingFestival, "Qingming Festival")]
+    [InlineData(ChineseSolarTerms.SummerSolstice, "Summer Solstice")]
+    public void GetEnglishName_ValidTerms_ReturnsCorrectName(ChineseSolarTerms term, string expected)
+    {
+        // Act
+        var result = ChineseSolarTermHelper.GetEnglishName(term);
+
+        // Assert
+        result.ShouldBe(expected);
+    }
+
+    /// <summary>
+    /// 测试 - GetSolarTerm - 获取指定日期的节气
+    /// </summary>
+    [Theory]
+    [InlineData(2023, 2, 4, false, "立春")] // 2023年立春：2月4日
+    [InlineData(2023, 3, 21, false, "春分")] // 2023年春分：3月21日
+    [InlineData(2023, 6, 21, false, "夏至")] // 2023年夏至：6月21日
+    [InlineData(2023, 12, 22, false, "冬至")] // 2023年冬至：12月22日
+    public void GetSolarTerm_TermDay_ReturnsSolarTermName(int year, int month, int day, bool traditional, string expected)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = ChineseSolarTermHelper.GetSolarTerm(_calendar, date, traditional);
+
+        // Assert
+        result.ShouldBe(expected);
+    }
+
+    /// <summary>
+    /// 测试 - GetSolarTerm - 非节气日期返回空字符串
+    /// </summary>
+    [Fact]
+    public void GetSolarTerm_NonTermDay_ReturnsEmptyString()
+    {
+        // Arrange
+        var date = new DateTime(2023, 2, 10); // 非节气日
+
+        // Act
+        var result = ChineseSolarTermHelper.GetSolarTerm(_calendar, date);
+
+        // Assert
+        result.ShouldBe(string.Empty);
+    }
+
+    /// <summary>
+    /// 测试 - GetLastSolarTerm - 获取上一个节气
+    /// </summary>
+    [Theory]
+    [InlineData(2023, 2, 10, "立春")] // 立春后几天，上一个是立春
+    [InlineData(2023, 3, 25, "春分")] // 春分后几天，上一个是春分
+    public void GetLastSolarTerm_ValidDate_ReturnsLastTerm(int year, int month, int day, string expected)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = ChineseSolarTermHelper.GetLastSolarTerm(_calendar, date, out var termDate);
+
+        // Assert
+        result.ShouldBe(expected);
+        termDate.ShouldNotBe(default);
+        termDate.ShouldBeLessThan(date);
+    }
+
+    /// <summary>
+    /// 测试 - GetNextSolarTerm - 获取下一个节气
+    /// </summary>
+    [Theory]
+    [InlineData(2023, 1, 25, "立春")] // 立春前几天，下一个是立春
+    [InlineData(2023, 3, 15, "春分")] // 春分前几天，下一个是春分
+    public void GetNextSolarTerm_ValidDate_ReturnsNextTerm(int year, int month, int day, string expected)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = ChineseSolarTermHelper.GetNextSolarTerm(_calendar, date, out var termDate);
+
+        // Assert
+        result.ShouldBe(expected);
+        termDate.ShouldNotBe(default);
+        termDate.ShouldBeGreaterThan(date);
+    }
+
+    /// <summary>
+    /// 测试 - GetSolarTermEnum - 获取节气枚举
+    /// </summary>
+    [Theory]
+    [InlineData(2023, 2, 4, ChineseSolarTerms.BeginningOfSpring)] // 2023年立春
+    [InlineData(2023, 3, 21, ChineseSolarTerms.VernalEquinox)] // 2023年春分
+    public void GetSolarTermEnum_TermDay_ReturnsSolarTermEnum(int year, int month, int day, ChineseSolarTerms expected)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = ChineseSolarTermHelper.GetSolarTermEnum(_calendar, date);
+
+        // Assert
+        result.ShouldBe(expected);
+    }
+
+    /// <summary>
+    /// 测试 - GetLastSolarTermEnum - 获取上一个节气枚举
+    /// </summary>
+    [Fact]
+    public void GetLastSolarTermEnum_ValidDate_ReturnsLastTermEnum()
+    {
+        // Arrange
+        var date = new DateTime(2023, 2, 10); // 立春后几天
+
+        // Act
+        var result = ChineseSolarTermHelper.GetLastSolarTermEnum(_calendar, date, out var termDate);
+
+        // Assert
+        result.ShouldBe(ChineseSolarTerms.BeginningOfSpring);
+        termDate.ShouldNotBe(default);
+        termDate.ShouldBeLessThan(date);
+    }
+
+    /// <summary>
+    /// 测试 - GetNextSolarTermEnum - 获取下一个节气枚举
+    /// </summary>
+    [Fact]
+    public void GetNextSolarTermEnum_ValidDate_ReturnsNextTermEnum()
+    {
+        // Arrange
+        var date = new DateTime(2023, 3, 15); // 春分前几天
+
+        // Act
+        var result = ChineseSolarTermHelper.GetNextSolarTermEnum(_calendar, date, out var termDate);
+
+        // Assert
+        result.ShouldBe(ChineseSolarTerms.VernalEquinox);
+        termDate.ShouldNotBe(default);
+        termDate.ShouldBeGreaterThan(date);
+    }
+
+    /// <summary>
+    /// 测试 - 无效日期参数 - 抛出异常
+    /// </summary>
+    [Fact]
+    public void GetSolarTerm_NullCalendar_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var date = new DateTime(2023, 2, 4);
+
+        // Act & Assert
+        Should.Throw<ArgumentNullException>(() => ChineseSolarTermHelper.GetSolarTerm(null, date));
+    }
+
+    /// <summary>
+    /// 测试 - 日期超出范围 - 返回空结果
+    /// </summary>
+    [Theory]
+    [InlineData(1800, 1, 1)] // 早于基准日期
+    [InlineData(2200, 1, 1)] // 超出最大支持日期
+    public void GetSolarTerm_DateOutOfRange_ReturnsEmptyString(int year, int month, int day)
+    {
+        // Arrange
+        var date = new DateTime(year, month, day);
+
+        // Act
+        var result = ChineseSolarTermHelper.GetSolarTerm(_calendar, date);
+
+        // Assert
+        result.ShouldBe(string.Empty);
+    }
+}
